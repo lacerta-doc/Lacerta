bf3a0c4fb89ed1d8e55c9cfd81645a7a4bb83277
ろむねこ <nem@nem.x0.to>
2023-12-07 13:44:02 +0900

    WIP

--- app/src/main/java/one/nem/lacerta/MainActivity.java
+++ app/src/main/java/one/nem/lacerta/MainActivity.java
@@ -1,8 +1,14 @@
 package one.nem.lacerta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // デバッグ
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment =
+                (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+        // デバッグ
+
     }
 }
