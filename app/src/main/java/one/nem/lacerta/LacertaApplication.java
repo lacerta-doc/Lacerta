d1e3255219563a05faf20bee1a210f0ae5510b44
ろむねこ <nem@nem.x0.to>
2023-12-10 22:53:59 +0900

    カラーパレットを適用するようにした

--- app/src/main/java/one/nem/lacerta/LacertaApplication.java
+++ app/src/main/java/one/nem/lacerta/LacertaApplication.java
@@ -2,8 +2,15 @@ package one.nem.lacerta;
 
 import android.app.Application;
 
+import com.google.android.material.color.DynamicColors;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
 public class LacertaApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
 }
