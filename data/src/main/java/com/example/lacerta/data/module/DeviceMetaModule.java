15ee6f344f4d4bbd451ae98536bdfdb6b2961b64
ろむねこ <nem@nem.x0.to>
2023-12-10 18:14:10 +0900

    HiltModule追加, Bind追加

--- data/src/main/java/com/example/lacerta/data/module/DeviceMetaModule.java
+++ data/src/main/java/com/example/lacerta/data/module/DeviceMetaModule.java
@@ -1,4 +1,21 @@
 package com.example.lacerta.data.module;
 
-public class DeviceMetaModule {
+import android.app.Application;
+
+import com.example.lacerta.data.impl.DeviceMetaImpl;
+import com.example.lacerta.data.repository.DeviceMeta;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+// Singleton
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
+abstract class DeviceMetaModule {
+
+    @Binds
+    public abstract DeviceMeta bindDeviceMeta(DeviceMetaImpl deviceMetaImpl);
+
 }
