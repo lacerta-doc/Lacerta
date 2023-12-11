7d158bf3e216cc5512b213ec530a98e6181c7c70
ろむねこ <nem@nem.x0.to>
2023-12-11 13:48:21 +0900

    ListからArrayListに変更, スタブを追加

--- data/src/main/java/one/nem/lacerta/data/impl/DocumentsImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/DocumentsImpl.java
@@ -1,4 +1,20 @@
 package one.nem.lacerta.data.impl;
 
-public class DocumentsImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import one.nem.lacerta.data.model.documents.DocumentMeta;
+import one.nem.lacerta.data.repository.Documents;
+
+public class DocumentsImpl implements Documents {
+
+    @Inject
+    public DocumentsImpl() {
+    }
+
+    public ArrayList<DocumentMeta> getRecentDocuments(int limit) {
+        return null; // TODO-rca:
+    }
 }
--- data/src/main/java/one/nem/lacerta/data/repository/Documents.java
+++ data/src/main/java/one/nem/lacerta/data/repository/Documents.java
@@ -1,5 +1,14 @@
 package one.nem.lacerta.data.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import one.nem.lacerta.data.model.documents.DocumentDetail;
+import one.nem.lacerta.data.model.documents.DocumentMeta;
+
 public interface Documents {
+
+    ArrayList<DocumentMeta> getRecentDocuments(int limit);
+
 
 }
