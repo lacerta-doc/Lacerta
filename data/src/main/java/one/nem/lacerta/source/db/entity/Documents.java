d7f73d9207c2823032cc9fec14adfd9bbc70fbc5
ろむねこ <nem@nem.x0.to>
2023-12-11 16:07:35 +0900

    WIP Entity作成

--- data/src/main/java/one/nem/lacerta/source/db/entity/Documents.java
+++ data/src/main/java/one/nem/lacerta/source/db/entity/Documents.java
@@ -1,4 +1,28 @@
 package one.nem.lacerta.source.db.entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+import one.nem.lacerta.data.model.documents.enums.DocumentType;
+
+@Entity
 public class Documents {
+    @PrimaryKey
+    private String id;
+
+    private String name; // Titleに変更する?
+
+    private DocumentType type; // DocumentTypeに変更する?
+
+    private Date created;
+
+    private String[] tags;
+
+    private String[] categories;
+
+    private String repositoryId;
+
+    // WIP
 }
