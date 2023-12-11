dc05e809f5a17c2b1b5e28ec30da43a0e4843bd5
ろむねこ <nem@nem.x0.to>
2023-12-11 13:49:32 +0900

    スタブをInjectionするようにした

--- data/src/main/java/one/nem/lacerta/data/module/DocumentsModule.java
+++ data/src/main/java/one/nem/lacerta/data/module/DocumentsModule.java
@@ -1,4 +1,18 @@
 package one.nem.lacerta.data.module;
 
-public class DocumentsModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+import one.nem.lacerta.data.repository.Documents;
+import one.nem.lacerta.data.impl.DocumentsImpl;
+
+@Module
+@InstallIn(SingletonComponent.class)
+abstract public class DocumentsModule {
+
+    @Binds
+    public abstract Documents bindDocuments(DocumentsImpl documentsImpl);
+
 }
