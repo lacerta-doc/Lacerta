07b64346891abd6649b6d3ea8ff0f74183576ae7
ろむねこ <nem@nem.x0.to>
2023-12-11 14:36:08 +0900

    Impl

--- data/src/main/java/one/nem/lacerta/data/impl/stub/DocumentsImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/stub/DocumentsImpl.java
@@ -37,4 +37,20 @@ public class DocumentsImpl implements Documents {
             return documentMetaList;
         }
 
+        public ArrayList<DocumentMeta> getStarredDocuments(int limit) { // Generate dummy data
+            ArrayList<DocumentMeta> documentMetaList = new ArrayList<>();
+            for (int i = 0; i < limit; i++) {
+                DocumentMeta documentMeta = new DocumentMeta();
+                documentMeta.id = UUID.randomUUID().toString();
+                documentMeta.name = "Document " + i;
+                documentMeta.created = new Date();
+                documentMeta.type = new Random(i).nextInt()/2 == 0 ? DocumentType.OTHER : DocumentType.NOTEBOOK;
+                documentMeta.tags = new String[] {"tag1", "tag2", "tag3"};
+                documentMeta.categories = new String[] {"category1", "category2", "category3"};
+                documentMetaList.add(documentMeta);
+            }
+
+            return documentMetaList;
+        }
+
 }
