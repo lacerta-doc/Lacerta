fd04610e6c8eda074203be561f5e303423d3d578
ろむねこ <nem@nem.x0.to>
2023-12-11 10:50:28 +0900

    WIP Binds

--- data/src/main/java/one/nem/lacerta/source/pref/module/CommonModule.java
+++ data/src/main/java/one/nem/lacerta/source/pref/module/CommonModule.java
@@ -1,4 +1,16 @@
 package one.nem.lacerta.source.pref.module;
 
-public class CommonModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import one.nem.lacerta.source.pref.impl.CommonImpl;
+import one.nem.lacerta.source.pref.repository.Common;
+
+@Module
+@InstallIn(SingletonComponent.class)
+abstract public class CommonModule {
+
+    @Binds
+    public abstract Common bindCommon(CommonImpl commonImpl);
 }
