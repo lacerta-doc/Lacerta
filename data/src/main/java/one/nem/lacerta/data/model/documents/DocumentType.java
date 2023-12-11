f1090e046dd7349ee074579db328dcac71e966bc
ろむねこ <nem@nem.x0.to>
2023-12-11 10:00:36 +0900

    ドキュメントタイプWIP

--- data/src/main/java/one/nem/lacerta/data/model/documents/DocumentType.java
+++ data/src/main/java/one/nem/lacerta/data/model/documents/DocumentType.java
@@ -1,4 +1,21 @@
 package one.nem.lacerta.data.model.documents;
 
-public class DocumentType {
+public enum DocumentType {
+    // ドキュメントの種類
+    // TODO: ユーザーがドキュメントの種類を追加できるようにする
+
+    // テキスト
+    TEXTBOOK,
+    // ノート
+    NOTEBOOK,
+    // ワークブック
+    WORKBOOK,
+    // レシート
+    RECEIPT,
+    // チケット
+    TICKET,
+    // メモ
+    MEMO,
+    // その他
+    OTHER
 }
