e37328e1f50b65290f13005d649e7fcd04f5a893
ろむねこ <nem@nem.x0.to>
2023-12-11 10:38:50 +0900

    CommonPref Interface実装

--- data/src/main/java/one/nem/lacerta/source/pref/repository/Common.java
+++ data/src/main/java/one/nem/lacerta/source/pref/repository/Common.java
@@ -1,5 +1,17 @@
 package one.nem.lacerta.source.pref.repository;
 
+import java.util.List;
+
 public interface Common {
     // さまざまな用途で使うPref
+
+    String getStringValue(String key);
+
+    void setStringValue(String key, String value);
+
+    boolean isExist(String key);
+
+    void remove(String key);
+
+    List<String> getExistKeys();
 }
