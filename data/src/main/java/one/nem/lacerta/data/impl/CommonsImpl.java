8f7a420ae363933f692cb4a6e588e6a4a6cba3eb
ろむねこ <nem@nem.x0.to>
2023-12-12 10:22:28 +0900

    RequireNonNullした

--- data/src/main/java/one/nem/lacerta/data/impl/CommonsImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/CommonsImpl.java
@@ -3,6 +3,7 @@ package one.nem.lacerta.data.impl;
 import android.content.Context;
 
 import java.nio.file.Path;
+import java.util.Objects;
 
 import javax.inject.Inject;
 
@@ -20,11 +21,11 @@ public class CommonsImpl implements Commons{
 
 
     public Path getExternalFilesDirPath(String type) {
-        return applicationContext.getExternalFilesDir(type).toPath();
+        return Objects.requireNonNull(applicationContext.getExternalFilesDir(type)).toPath();
     }
 
     public Path getExternalFilesDirPath() {
-        return applicationContext.getExternalFilesDir(null).toPath();
+        return Objects.requireNonNull(applicationContext.getExternalFilesDir(null)).toPath();
     }
 
 }
