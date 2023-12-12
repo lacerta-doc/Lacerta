c7829609497cd05bd55219e84437055af564b3bf
ろむねこ <nem@nem.x0.to>
2023-12-12 11:37:11 +0900

    DebugFuncモジュール作成

--- data/src/main/java/one/nem/lacerta/data/module/DebugFuncModule.java
+++ data/src/main/java/one/nem/lacerta/data/module/DebugFuncModule.java
@@ -1,4 +1,15 @@
 package one.nem.lacerta.data.module;
 
-public class DebugFuncModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import one.nem.lacerta.data.repository.DebugFunc;
+import one.nem.lacerta.data.impl.DebugFuncImpl;
+
+@Module
+@InstallIn(DebugFuncImpl.class)
+abstract class DebugFuncModule {
+
+    @Binds
+    public abstract DebugFunc bindDebugFunc(DebugFuncImpl debugFuncImpl);
 }
