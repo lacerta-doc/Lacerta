065d3594ac70b1549d9e96995ebdccffb616a743
ろむねこ <nem@nem.x0.to>
2023-12-12 10:24:53 +0900

    Module実装

--- data/src/main/java/one/nem/lacerta/data/module/CommonsModule.java
+++ data/src/main/java/one/nem/lacerta/data/module/CommonsModule.java
@@ -1,4 +1,16 @@
 package one.nem.lacerta.data.module;
 
-public class CommonsModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import one.nem.lacerta.data.repository.Commons;
+import one.nem.lacerta.data.impl.CommonsImpl;
+
+@Module
+@InstallIn(SingletonComponent.class)
+abstract class CommonsModule {
+
+    @Binds
+    public abstract Commons bindCommons(CommonsImpl commonsImpl);
 }
