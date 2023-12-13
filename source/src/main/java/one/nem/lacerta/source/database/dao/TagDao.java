2ab89996389b6beb8fa2b806ac4cfc5fa5f68890
r-ca <nem@nem.x0.to>
2023-12-14 04:08:01 +0900

    TagDao: Insert, Update, Delete実装

--- source/src/main/java/one/nem/lacerta/source/database/dao/TagDao.java
+++ source/src/main/java/one/nem/lacerta/source/database/dao/TagDao.java
@@ -1,7 +1,10 @@
 package one.nem.lacerta.source.database.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -9,6 +12,8 @@ import one.nem.lacerta.source.database.entity.Tag;
 
 @Dao
 public interface TagDao {
+
+    // Select
 
     @Query("SELECT * FROM tag WHERE id = :id")
     Tag findById(String id);
@@ -19,6 +24,37 @@ public interface TagDao {
     @Query("SELECT * FROM tag WHERE id IN (:ids)")
     List<Tag> findByIds(List<String> ids);
 
-    // WIP
-    // TODO-rca: Insert, Update, Delete
+    // Insert
+    @Insert
+    void insert(Tag tag);
+
+    @Insert
+    void insertAll(Tag... tags);
+
+    @Insert
+    void insertAll(List<Tag> tags);
+
+    // Update
+    @Update
+    void update(Tag tag);
+
+    @Update
+    void updateAll(Tag... tags);
+
+    @Update
+    void updateAll(List<Tag> tags);
+
+    // Delete
+    @Delete
+    void delete(Tag tag);
+
+    @Delete
+    void deleteAll(Tag... tags);
+
+    @Delete
+    void deleteAll(List<Tag> tags);
+
+    @Delete
+    void deleteById(String id);
+
 }
