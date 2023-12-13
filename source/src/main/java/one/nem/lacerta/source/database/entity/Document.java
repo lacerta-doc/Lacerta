7c3588275cb8edb1ddb44e0bd18936171e9a4ca8
r-ca <nem@nem.x0.to>
2023-12-14 02:25:26 +0900

    source: WIP, Documentエンティティ作成

--- source/src/main/java/one/nem/lacerta/source/database/entity/Document.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/Document.java
@@ -1,4 +1,20 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "document")
 public class Document {
+    @PrimaryKey
+    public String id; // ドキュメントID
+
+    public String title; // タイトル
+
+    public String createdAt; // 作成日時
+
+    public String updatedAt; // 更新日時
+
+    public String author; // 作成者
+
+    public String defaultBranch; // デフォルトブランチ
 }
