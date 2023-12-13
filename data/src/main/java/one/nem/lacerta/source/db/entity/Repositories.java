ab4a88e4bd500b454088b6d189bb28472938f304
r-ca <nem@nem.x0.to>
2023-12-13 18:29:43 +0900

    リポジトリを管理するエンティティを作成 WIP

--- data/src/main/java/one/nem/lacerta/source/db/entity/Repositories.java
+++ data/src/main/java/one/nem/lacerta/source/db/entity/Repositories.java
@@ -1,4 +1,14 @@
 package one.nem.lacerta.source.db.entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Repositories {
+
+    @PrimaryKey
+    private String id;
+
+    private String relativePath;
+
 }
