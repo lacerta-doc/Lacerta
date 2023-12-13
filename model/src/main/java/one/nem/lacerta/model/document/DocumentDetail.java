250101c00ad1934eab289fe19e29d72c00825633
r-ca <nem@nem.x0.to>
2023-12-14 04:15:39 +0900

    DocumentDetailのモデル作成WIP

---
+++ model/src/main/java/one/nem/lacerta/model/document/DocumentDetail.java
@@ -0,0 +1,15 @@
+package one.nem.lacerta.model.document;
+
+import java.util.Date;
+
+public class DocumentDetail {
+
+    String id;
+    String title;
+    Date createdAt;
+    Date updatedAt;
+    String author;
+    String defaultBranch;
+
+    // TODO-rca: Getter, Setter
+}
