6f3639702aec8ece26ee0a45335145d18ebd7b99
r-ca <nem@nem.x0.to>
2023-12-14 03:15:44 +0900

    utils: WIP Implクラスに全部Implementした

--- utils/src/main/java/one/nem/lacerta/utils/impl/DeviceInfoUtilsImpl.java
+++ utils/src/main/java/one/nem/lacerta/utils/impl/DeviceInfoUtilsImpl.java
@@ -1,4 +1,28 @@
 package one.nem.lacerta.utils.impl;
 
-public class DeviceInfoUtilsImpl {
+import java.nio.file.Path;
+
+import one.nem.lacerta.utils.repository.DeviceInfoUtils;
+public class DeviceInfoUtilsImpl implements DeviceInfoUtils {
+
+
+    @Override
+    public Path getExternalStorageDirectory() {
+        return null;
+    }
+
+    @Override
+    public Path getExternalStorageDirectory(String type) {
+        return null;
+    }
+
+    @Override
+    public boolean hasPermission(String permission) {
+        return false;
+    }
+
+    @Override
+    public boolean hasPermissions(String[] permissions) {
+        return false;
+    }
 }
