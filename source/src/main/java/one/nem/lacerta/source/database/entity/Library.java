8d86197270663da4546502b1fc3cf5cb8ca1b638
r-ca <nem@nem.x0.to>
2023-12-14 02:28:32 +0900

    source: WIP, Libraryエンティティ実装

--- source/src/main/java/one/nem/lacerta/source/database/entity/Library.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/Library.java
@@ -1,4 +1,15 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "library")
 public class Library {
+
+    @PrimaryKey
+    public String id; // ドキュメントID
+
+    public String rootPath; // rootのパス
+
+    public String path; // パス
 }
