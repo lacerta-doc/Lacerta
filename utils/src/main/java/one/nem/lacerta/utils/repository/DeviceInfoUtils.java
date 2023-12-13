62247033e946d26d5891347c33181999c856ef13
r-ca <nem@nem.x0.to>
2023-12-14 03:14:55 +0900

    utils: WIP リポジトリ実装

--- utils/src/main/java/one/nem/lacerta/utils/repository/DeviceInfoUtils.java
+++ utils/src/main/java/one/nem/lacerta/utils/repository/DeviceInfoUtils.java
@@ -1,4 +1,17 @@
 package one.nem.lacerta.utils.repository;
 
+import java.nio.file.Path;
+
 public interface DeviceInfoUtils {
+
+    // External storage directory
+    Path getExternalStorageDirectory();
+
+    Path getExternalStorageDirectory(String type);
+
+    // Permission
+    boolean hasPermission(String permission);
+
+    boolean hasPermissions(String[] permissions);
+
 }
