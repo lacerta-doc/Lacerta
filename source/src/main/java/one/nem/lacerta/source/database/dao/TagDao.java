2502cf64ec3429d1fd01c67cbff840d389bcc59c
r-ca <nem@nem.x0.to>
2023-12-14 02:43:50 +0900

    source: Dao: WIP, TagDao実装

--- source/src/main/java/one/nem/lacerta/source/database/dao/TagDao.java
+++ source/src/main/java/one/nem/lacerta/source/database/dao/TagDao.java
@@ -1,4 +1,24 @@
 package one.nem.lacerta.source.database.dao;
 
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+import one.nem.lacerta.source.database.entity.Tag;
+
+@Dao
 public interface TagDao {
+
+    @Query("SELECT * FROM tag WHERE id = :id")
+    Tag findById(String id);
+
+    @Query("SELECT * FROM tag")
+    List<Tag> findAll();
+
+    @Query("SELECT * FROM tag WHERE id IN (:ids)")
+    List<Tag> findByIds(List<String> ids);
+
+    // WIP
+    // TODO-rca: Insert, Update, Delete
 }
