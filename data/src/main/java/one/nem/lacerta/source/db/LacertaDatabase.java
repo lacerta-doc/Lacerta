daef06810bd4b0e42e6e6a0d7d184ff777ee18e6
r-ca <nem@nem.x0.to>
2023-12-13 18:43:16 +0900

    Databaseクラス作成WIP

---
+++ data/src/main/java/one/nem/lacerta/source/db/LacertaDatabase.java
@@ -0,0 +1,14 @@
+package one.nem.lacerta.source.db;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import one.nem.lacerta.source.db.dao.DocumentsDao;
+import one.nem.lacerta.source.db.entity.Documents;
+import one.nem.lacerta.source.db.entity.Repositories;
+
+@Database(entities = {Documents.class, Repositories.class}, version = 1)
+public abstract class LacertaDatabase extends RoomDatabase {
+    public abstract DocumentsDao documentsDao();
+    public abstract RepositoriesDao repositoriesDao();
+}
