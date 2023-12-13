17bf162341898c1d2c9b11e9c303dcf0cc47a5b4
r-ca <nem@nem.x0.to>
2023-12-14 04:28:10 +0900

    フルパスを返すGetterを追加

--- model/src/main/java/one/nem/lacerta/model/document/path/DocumentPath.java
+++ model/src/main/java/one/nem/lacerta/model/document/path/DocumentPath.java
@@ -1,4 +1,41 @@
 package one.nem.lacerta.model.document.path;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DocumentPath {
+
+        String rootPath;
+        String path;
+
+        // Getter
+
+        public String getRootPath() {
+            return rootPath;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public Path getFullPath() {
+            // rootPathとpathを結合して返す
+            return Paths.get(rootPath, path);
+        }
+
+        public String getFullPathString() {
+            // rootPathとpathを結合して返す
+            return Paths.get(rootPath, path).toString();
+        }
+
+        // Setter
+
+        public void setRootPath(String rootPath) {
+            this.rootPath = rootPath;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
 }
