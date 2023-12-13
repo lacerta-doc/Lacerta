e9689bb99a21691ed469992138d0f7301ae99162
r-ca <nem@nem.x0.to>
2023-12-13 18:33:50 +0900

    Documentsエンティティ修正

--- data/src/main/java/one/nem/lacerta/source/db/entity/Documents.java
+++ data/src/main/java/one/nem/lacerta/source/db/entity/Documents.java
@@ -1,5 +1,6 @@
 package one.nem.lacerta.source.db.entity;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -12,14 +13,22 @@ public class Documents {
     @PrimaryKey
     private String id;
 
-    private String name; // Titleに変更する?
+    @ColumnInfo(name = "name")
+    private String title; // Titleに変更する?
 
+    @ColumnInfo(name = "type")
     private DocumentType type; // DocumentTypeに変更する?
 
+    @ColumnInfo(name = "created")
     private Date created;
 
+    @ColumnInfo(name = "updated")
+    private Date updated;
+
+    @ColumnInfo(name = "tags")
     private String[] tags;
 
+    @ColumnInfo(name = "categories")
     private String[] categories;
 
     // WIP
