984b82d4a1714841f37d2abc7ee89f364b406bbd
r-ca <nem@nem.x0.to>
2023-12-14 02:26:43 +0900

    source: WIP, 列名の指定を追加した

--- source/src/main/java/one/nem/lacerta/source/database/entity/Document.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/Document.java
@@ -1,20 +1,27 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "document")
 public class Document {
     @PrimaryKey
+    @ColumnInfo(name = "id")
     public String id; // ドキュメントID
 
+    @ColumnInfo(name = "title")
     public String title; // タイトル
 
+    @ColumnInfo(name = "created_at")
     public String createdAt; // 作成日時
 
+    @ColumnInfo(name = "updated_at")
     public String updatedAt; // 更新日時
 
+    @ColumnInfo(name = "author")
     public String author; // 作成者
 
+    @ColumnInfo(name = "default_branch")
     public String defaultBranch; // デフォルトブランチ
 }
