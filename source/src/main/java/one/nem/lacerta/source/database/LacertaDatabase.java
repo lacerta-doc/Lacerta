26df200fe461170b5e35e22ce204ec6bf709f113
r-ca <nem@nem.x0.to>
2023-12-14 02:58:48 +0900

    source: WIP Database

--- source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
+++ source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
@@ -1,4 +1,20 @@
 package one.nem.lacerta.source.database;
 
-public class LacertaDatabase {
+import androidx.room.Database;
+
+// Entities
+import one.nem.lacerta.source.database.entity.Tag;
+import one.nem.lacerta.source.database.entity.Document;
+import one.nem.lacerta.source.database.entity.Library;
+
+// Daos
+import one.nem.lacerta.source.database.dao.TagDao;
+import one.nem.lacerta.source.database.dao.DocumentDao;
+import one.nem.lacerta.source.database.dao.LibraryDao;
+
+@Database(entities = {Tag.class, Document.class, Library.class}, version = 1)
+public abstract class LacertaDatabase {
+    public abstract TagDao tagDao();
+    public abstract DocumentDao documentDao();
+    public abstract LibraryDao libraryDao();
 }
