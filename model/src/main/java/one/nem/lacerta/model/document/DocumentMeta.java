d4f1d76763b3319d2f9ddf0fc643267253602b28
r-ca <nem@nem.x0.to>
2023-12-14 04:17:13 +0900

    DocumentMetaのモデル作成WIP

---
+++ model/src/main/java/one/nem/lacerta/model/document/DocumentMeta.java
@@ -0,0 +1,14 @@
+package one.nem.lacerta.model.document;
+
+import java.util.Date;
+
+public class DocumentMeta {
+
+        String id;
+        String title;
+        Date updatedAt;
+        String author;
+        String defaultBranch;
+
+        // TODO-rca: Getter, Setter
+}
