2ccc576c69ae7a532224cd11b7aa6d7f565b6762
r-ca <nem@nem.x0.to>
2023-12-14 02:42:25 +0900

    source: Dao: WIP, LibraryDao実装

--- source/src/main/java/one/nem/lacerta/source/database/dao/LibraryDao.java
+++ source/src/main/java/one/nem/lacerta/source/database/dao/LibraryDao.java
@@ -1,4 +1,24 @@
 package one.nem.lacerta.source.database.dao;
 
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+import one.nem.lacerta.source.database.entity.Library;
+
+@Dao
 public interface LibraryDao {
+
+    @Query("SELECT * FROM library WHERE id = :id")
+    Library findById(String id);
+
+    @Query("SELECT * FROM library")
+    List<Library> findAll();
+
+    @Query("SELECT * FROM library WHERE id IN (:ids)")
+    List<Library> findByIds(List<String> ids);
+
+    // WIP
+    // TODO-rca: Insert, Update, Delete
 }
