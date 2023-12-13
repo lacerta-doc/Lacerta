8a789ffe7dcc510e5e918010d136bcd6d27386b1
r-ca <nem@nem.x0.to>
2023-12-14 02:59:32 +0900

    source: WIP Database アノテーション付与

--- source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
+++ source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
@@ -3,6 +3,8 @@ package one.nem.lacerta.source.database;
 import androidx.room.Database;
 
 // Entities
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
 import one.nem.lacerta.source.database.entity.Tag;
 import one.nem.lacerta.source.database.entity.Document;
 import one.nem.lacerta.source.database.entity.Library;
@@ -13,6 +15,7 @@ import one.nem.lacerta.source.database.dao.DocumentDao;
 import one.nem.lacerta.source.database.dao.LibraryDao;
 
 @Database(entities = {Tag.class, Document.class, Library.class}, version = 1)
+@InstallIn(SingletonComponent.class)
 public abstract class LacertaDatabase {
     public abstract TagDao tagDao();
     public abstract DocumentDao documentDao();
