17cb6af538d9234ca0908531ea4b42d4b5750e1d
r-ca <nem@nem.x0.to>
2023-12-14 03:27:48 +0900

    変更

--- source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
+++ source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
@@ -3,8 +3,6 @@ package one.nem.lacerta.source.database;
 import androidx.room.Database;
 
 // Entities
-import dagger.hilt.InstallIn;
-import dagger.hilt.components.SingletonComponent;
 import one.nem.lacerta.source.database.entity.Tag;
 import one.nem.lacerta.source.database.entity.Document;
 import one.nem.lacerta.source.database.entity.Library;
@@ -15,7 +13,6 @@ import one.nem.lacerta.source.database.dao.DocumentDao;
 import one.nem.lacerta.source.database.dao.LibraryDao;
 
 @Database(entities = {Tag.class, Document.class, Library.class}, version = 1)
-@InstallIn(SingletonComponent.class)
 public abstract class LacertaDatabase {
     public abstract TagDao tagDao();
     public abstract DocumentDao documentDao();
