8a0b6bd3db2093d99244c7b38c07e08c97141c7c
r-ca <nem@nem.x0.to>
2023-12-14 03:48:22 +0900

    Hiltモジュール実装

--- utils/src/main/java/one/nem/lacerta/utils/module/DeviceInfoUtilsModule.java
+++ utils/src/main/java/one/nem/lacerta/utils/module/DeviceInfoUtilsModule.java
@@ -1,4 +1,16 @@
 package one.nem.lacerta.utils.module;
 
-public class DeviceInfoUtilsModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import one.nem.lacerta.utils.repository.DeviceInfoUtils;
+import one.nem.lacerta.utils.impl.DeviceInfoUtilsImpl;
+
+@Module
+@InstallIn(SingletonComponent.class) // TODO-rca: Singletonである必要があるのか検討する?
+abstract public class DeviceInfoUtilsModule {
+
+    @Binds
+    public abstract DeviceInfoUtils bindDeviceInfoUtils(DeviceInfoUtilsImpl deviceInfoUtilsImpl);
 }
