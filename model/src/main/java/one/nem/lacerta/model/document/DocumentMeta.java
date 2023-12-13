bacab14be6e7392fb49305e60d0dd487481bbaa1
r-ca <nem@nem.x0.to>
2023-12-14 04:33:20 +0900

    Getter/Setter

--- model/src/main/java/one/nem/lacerta/model/document/DocumentMeta.java
+++ model/src/main/java/one/nem/lacerta/model/document/DocumentMeta.java
@@ -12,5 +12,47 @@ public class DocumentMeta {
         Date createdAt;
         ArrayList<DocumentTag> tags;
 
-        // TODO-rca: Getter, Setter
+        // Getter
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Date getUpdatedAt() {
+            return updatedAt;
+        }
+
+        public Date getCreatedAt() {
+            return createdAt;
+        }
+
+        public ArrayList<DocumentTag> getTags() {
+            return tags;
+        }
+
+        // Setter
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setUpdatedAt(Date updatedAt) {
+            this.updatedAt = updatedAt;
+        }
+
+        public void setCreatedAt(Date createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public void setTags(ArrayList<DocumentTag> tags) {
+            this.tags = tags;
+        }
 }
