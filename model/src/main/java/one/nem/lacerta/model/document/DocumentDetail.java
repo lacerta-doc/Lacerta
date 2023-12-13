2b04432a37a2f9cf87222f82378e52ccfd0f3268
r-ca <nem@nem.x0.to>
2023-12-14 04:24:38 +0900

    Getter/Setter作成

--- model/src/main/java/one/nem/lacerta/model/document/DocumentDetail.java
+++ model/src/main/java/one/nem/lacerta/model/document/DocumentDetail.java
@@ -11,5 +11,57 @@ public class DocumentDetail {
     String author;
     String defaultBranch;
 
-    // TODO-rca: Getter, Setter
+    // Getter
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDefaultBranch() {
+        return defaultBranch;
+    }
+
+    // Setter
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDefaultBranch(String defaultBranch) {
+        this.defaultBranch = defaultBranch;
+    }
+
+
 }
