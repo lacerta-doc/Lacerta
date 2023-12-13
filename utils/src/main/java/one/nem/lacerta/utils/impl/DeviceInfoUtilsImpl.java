2155edd15fa173d7b8839b5485bdca80f29bd71e
r-ca <nem@nem.x0.to>
2023-12-14 03:17:26 +0900

    utils: アプリのコンテキストを取得するようにした（アノテーション忘れ）

--- utils/src/main/java/one/nem/lacerta/utils/impl/DeviceInfoUtilsImpl.java
+++ utils/src/main/java/one/nem/lacerta/utils/impl/DeviceInfoUtilsImpl.java
@@ -4,19 +4,22 @@ import android.content.Context;
 
 import java.nio.file.Path;
 
+import javax.inject.Inject;
+
 import dagger.hilt.android.qualifiers.ApplicationContext;
 import one.nem.lacerta.utils.repository.DeviceInfoUtils;
 public class DeviceInfoUtilsImpl implements DeviceInfoUtils {
 
     private final Context applicationContext;
 
+    @Inject
     public DeviceInfoUtilsImpl(@ApplicationContext Context applicationContext) {
         this.applicationContext = applicationContext;
     }
 
     @Override
     public Path getExternalStorageDirectory() {
-        return null;
+        return
     }
 
     @Override
