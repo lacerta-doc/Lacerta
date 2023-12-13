49cc132f14818f1bf01a0d9fab8885af35abf1f7
r-ca <nem@nem.x0.to>
2023-12-14 03:41:33 +0900

    NonNullアノテーション追加

--- source/src/main/java/one/nem/lacerta/source/database/entity/Library.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/Library.java
@@ -1,5 +1,6 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -9,6 +10,7 @@ public class Library {
 
     @PrimaryKey
     @ColumnInfo(name = "id")
+    @NonNull
     public String id; // ドキュメントID
 
     @ColumnInfo(name = "root_path")
