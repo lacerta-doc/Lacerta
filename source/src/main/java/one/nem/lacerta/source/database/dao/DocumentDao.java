8f118b7067bab6f0f951f5882fee12ee827a4fb2
r-ca <nem@nem.x0.to>
2023-12-14 04:06:54 +0900

    DocumentDao: Insert, Update, Delete実装

--- source/src/main/java/one/nem/lacerta/source/database/dao/DocumentDao.java
+++ source/src/main/java/one/nem/lacerta/source/database/dao/DocumentDao.java
@@ -1,7 +1,10 @@
 package one.nem.lacerta.source.database.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -11,6 +14,8 @@ import one.nem.lacerta.source.database.entity.Document;
 @Dao
 public interface DocumentDao {
 
+    // Select
+
     @Query("SELECT * FROM document WHERE id = :id")
     Document findById(String id);
 
@@ -18,10 +23,40 @@ public interface DocumentDao {
     List<Document> findAll();
 
     @Query("SELECT * FROM document WHERE id IN (:ids)")
-
     List<Document> findByIds(List<String> ids);
 
-    // WIP
-    // TODO-rca: Insert, Update, Delete
+    // Insert
+    @Insert
+    void insert(Document document);
+
+    @Insert
+    void insertAll(Document... documents);
+
+    @Insert
+    void insertAll(List<Document> documents);
+
+    // Update
+
+    @Update
+    void update(Document document);
+
+    @Update
+    void updateAll(Document... documents);
+
+    @Update
+    void updateAll(List<Document> documents);
+
+    // Delete
+    @Delete
+    void delete(Document document);
+
+    @Delete
+    void deleteAll(Document... documents);
+
+    @Delete
+    void deleteAll(List<Document> documents);
+
+    @Delete
+    void deleteById(String id);
 
 }
