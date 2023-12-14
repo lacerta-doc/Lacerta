72163182a0e38b5cf6c094e97450d55d56765609
ろむねこ <nem@nem.x0.to>
2023-12-14 14:29:19 +0900

    Impl WIP

--- source/src/main/java/one/nem/lacerta/source/jgit/impl/ManageRepoImpl.java
+++ source/src/main/java/one/nem/lacerta/source/jgit/impl/ManageRepoImpl.java
@@ -1,4 +1,29 @@
 package one.nem.lacerta.source.jgit.impl;
 
-public class ManageRepoImpl {
+import one.nem.lacerta.source.jgit.ManageRepo;
+
+import javax.inject.Inject;
+
+import org.eclipse.jgit.lib.Repository;
+public class ManageRepoImpl implements ManageRepo {
+
+    @Override
+    public Repository getRepository(String id) {
+        return null;
+    }
+
+    @Override
+    public Repository createRepository(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteRepository(String id) {
+
+    }
+
+    @Override
+    public boolean repositoryExists(String id) {
+        return false;
+    }
 }
