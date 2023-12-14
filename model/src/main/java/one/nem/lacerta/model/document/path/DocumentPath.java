b731e566e725377bd6f4e37c5eca461377dd24de
ろむねこ <nem@nem.x0.to>
2023-12-14 12:51:15 +0900

    JavaDoc対応

--- model/src/main/java/one/nem/lacerta/model/document/path/DocumentPath.java
+++ model/src/main/java/one/nem/lacerta/model/document/path/DocumentPath.java
@@ -3,49 +3,79 @@ package one.nem.lacerta.model.document.path;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * ドキュメントのパス
+ */
 public class DocumentPath {
 
-        String rootPath;
-        String path;
+    /**
+     * ドキュメントのルートパス(String)
+     */
+    String rootPath;
 
-        // Constructor
+    /**
+     * ドキュメントのパス(String)
+     */
+    String path;
 
-        public DocumentPath() {
-        }
+    // Constructor
 
-        public DocumentPath(String rootPath, String path) {
-            this.rootPath = rootPath;
-            this.path = path;
-        }
+    public DocumentPath() {
+    }
 
-        // Getter
+    public DocumentPath(String rootPath, String path) {
+        this.rootPath = rootPath;
+        this.path = path;
+    }
 
-        public String getRootPath() {
-            return rootPath;
-        }
+    // Getter
 
-        public String getPath() {
-            return path;
-        }
+    /**
+     * ドキュメントのルートパス(String)を取得する
+     */
+    public String getRootPath() {
+        return rootPath;
+    }
 
-        public Path getFullPath() {
-            // rootPathとpathを結合して返す
-            return Paths.get(rootPath, path);
-        }
+    /**
+     * ドキュメントのパス(String)を取得する
+     */
+    public String getPath() {
+        return path;
+    }
 
-        public String getFullPathString() {
-            // rootPathとpathを結合して返す
-            return Paths.get(rootPath, path).toString();
-        }
+    /**
+     * ドキュメントのフルパス(Path)を取得する
+     */
+    public Path getFullPath() {
+        // rootPathとpathを結合して返す
+        return Paths.get(rootPath, path);
+    }
 
-        // Setter
+    /**
+     * ドキュメントのフルパス(String)を取得する
+     */
+    public String getFullPathString() {
+        // rootPathとpathを結合して返す
+        return Paths.get(rootPath, path).toString();
+    }
 
-        public void setRootPath(String rootPath) {
-            this.rootPath = rootPath;
-        }
+    // Setter
 
-        public void setPath(String path) {
-            this.path = path;
-        }
+    /**
+     * ドキュメントのルートパス(String)を設定する
+     * @param rootPath ドキュメントのルートパス
+     */
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    /**
+     * ドキュメントのパス(String)を設定する
+     * @param path ドキュメントのパス
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
 
 }
