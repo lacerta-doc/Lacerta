00983d49cd98f102e6a636ce7c52c41619c445a5
ろむねこ <nem@nem.x0.to>
2023-12-14 11:46:50 +0900

    DocumentDebugインターフェースWIP

--- data/src/main/java/one/nem/lacerta/data/DocumentDebug.java
+++ data/src/main/java/one/nem/lacerta/data/DocumentDebug.java
@@ -1,4 +1,12 @@
 package one.nem.lacerta.data;
 
+import one.nem.lacerta.model.document.DocumentMeta;
+import one.nem.lacerta.model.document.DocumentDetail;
+import one.nem.lacerta.model.document.path.DocumentPath;
+import one.nem.lacerta.model.document.tag.DocumentTag;
+
 public interface DocumentDebug {
+
+    void insertDocument(DocumentMeta meta, DocumentDetail detail);
+
 }
