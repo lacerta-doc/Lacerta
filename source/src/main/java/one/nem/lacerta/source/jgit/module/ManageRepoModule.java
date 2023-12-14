12cba0242b49f0eaf7d92b0d0ac7f243fada16f0
ろむねこ <nem@nem.x0.to>
2023-12-14 14:38:34 +0900

    Hilt Module実装

--- source/src/main/java/one/nem/lacerta/source/jgit/module/ManageRepoModule.java
+++ source/src/main/java/one/nem/lacerta/source/jgit/module/ManageRepoModule.java
@@ -1,4 +1,16 @@
 package one.nem.lacerta.source.jgit.module;
 
-public class ManageRepoModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import one.nem.lacerta.source.jgit.ManageRepo;
+import one.nem.lacerta.source.jgit.impl.ManageRepoImpl;
+
+@Module
+@InstallIn(SingletonComponent.class)
+abstract public class ManageRepoModule {
+
+    @Binds
+    public abstract ManageRepo bindManageRepo(ManageRepoImpl manageRepoImpl);
 }
