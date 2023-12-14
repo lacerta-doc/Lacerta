243720ee96a4eb17b5a925e67b0ea862695fc577
ろむねこ <nem@nem.x0.to>
2023-12-14 12:37:19 +0900

    JavaDoc対応

--- data/src/main/java/one/nem/lacerta/data/Document.java
+++ data/src/main/java/one/nem/lacerta/data/Document.java
@@ -7,14 +7,34 @@ import one.nem.lacerta.model.document.DocumentDetail;
 import one.nem.lacerta.model.document.path.DocumentPath;
 import one.nem.lacerta.model.document.tag.DocumentTag;
 
+/**
+ * ドキュメントのデータを取得する
+ */
 public interface Document {
 
+    /**
+     * 更新の新しいドキュメントから順に並べてlimit件取得する
+     * @param limit 取得する上限数
+     */
     ArrayList<DocumentMeta> getRecentDocumentMetas(int limit);
 
+    /**
+     * 更新の新しいドキュメントから順に並べてoffset位置からlimit件取得する
+     * @param limit 取得する上限数
+     * @param offset 取得するオフセット
+     */
     ArrayList<DocumentMeta> getRecentDocumentMetas(int limit, int offset);
 
-    // ドキュメント詳細取得
+    /**
+     * ドキュメントIDからDocumentDetailを取得する
+     * @param id ドキュメントID
+     */
     DocumentDetail getDocumentDetail(String id);
+
+    /**
+     * DocumentMetaからDocumentDetailを取得する
+     * @param meta DocumentMeta
+     */
     DocumentDetail getDocumentDetailByMeta(DocumentMeta meta); // 簡単に使えるように
 
 
