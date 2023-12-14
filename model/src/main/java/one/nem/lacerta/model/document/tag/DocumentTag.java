992caa40884369fa7d377bf4f1e3adf1d6f6f4ea
ろむねこ <nem@nem.x0.to>
2023-12-14 12:48:44 +0900

    JavaDoc対応

--- model/src/main/java/one/nem/lacerta/model/document/tag/DocumentTag.java
+++ model/src/main/java/one/nem/lacerta/model/document/tag/DocumentTag.java
@@ -1,35 +1,67 @@
 package one.nem.lacerta.model.document.tag;
 
+/**
+ * ドキュメントのタグ
+ */
 public class DocumentTag {
 
+    /**
+     * タグのID(String)
+     */
     String id;
+
+    /**
+     * タグの名前(String)
+     */
     String name;
+
+    /**
+     * タグの色(String)
+     */
     String color;
 
     // Getter
 
+    /**
+     * タグのID(String)を取得する
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * タグの名前(String)を取得する
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * タグの色(String)を取得する
+     */
     public String getColor() {
         return color;
     }
 
     // Setter
 
+    /**
+     * タグのID(String)を設定する
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * タグの名前(String)を設定する
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * タグの色(String)を設定する
+     */
     public void setColor(String color) {
         this.color = color;
     }
