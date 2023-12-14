8196e9afcba711e2ff6c33f83f12690e840d238f
ろむねこ <nem@nem.x0.to>
2023-12-14 11:32:22 +0900

    DocumentのHiltモジュールを実装した

--- data/src/main/java/one/nem/lacerta/data/module/DocumentModule.java
+++ data/src/main/java/one/nem/lacerta/data/module/DocumentModule.java
@@ -1,4 +1,20 @@
 package one.nem.lacerta.data.module;
 
-public class DocumentModule {
+import android.content.Context;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+import one.nem.lacerta.data.Document;
+import one.nem.lacerta.data.impl.DocumentImpl;
+
+@Module
+@InstallIn(SingletonComponent.class) // TODO-rca: Singletonであるべきか考える
+abstract public class DocumentModule {
+
+    @Binds
+    public abstract Document bindDocument(DocumentImpl documentImpl);
+
 }
