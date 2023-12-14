cba87159ab9948ccc08105cac584c46bc72eaf37
ろむねこ <nem@nem.x0.to>
2023-12-14 10:42:22 +0900

    data: DocumentリポジトリのImpl WIP

--- data/src/main/java/one/nem/lacerta/data/impl/DocumentImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/DocumentImpl.java
@@ -1,4 +1,35 @@
 package one.nem.lacerta.data.impl;
 
-public class DocumentImpl {
+import java.util.ArrayList;
+
+import one.nem.lacerta.data.Document;
+
+import one.nem.lacerta.model.document.DocumentMeta;
+import one.nem.lacerta.model.document.DocumentDetail;
+import one.nem.lacerta.model.document.path.DocumentPath;
+import one.nem.lacerta.model.document.tag.DocumentTag;
+
+
+public class DocumentImpl implements Document{
+
+
+    @Override
+    public ArrayList<DocumentMeta> getRecentDocumentMetas(int limit) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<DocumentMeta> getRecentDocumentMetas(int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    public DocumentDetail getDocumentDetail(String id) {
+        return null;
+    }
+
+    @Override
+    public DocumentDetail getDocumentDetailByMeta(DocumentMeta meta) {
+        return null;
+    }
 }
