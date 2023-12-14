1e3565405e841a1dad6b8b057fece989d8aac782
ろむねこ <nem@nem.x0.to>
2023-12-14 10:40:25 +0900

    data: DocumentリポジトリのインターフェースWIP

--- data/src/main/java/one/nem/lacerta/data/Document.java
+++ data/src/main/java/one/nem/lacerta/data/Document.java
@@ -1,4 +1,22 @@
 package one.nem.lacerta.data;
 
+import java.util.ArrayList;
+
+import one.nem.lacerta.model.document.DocumentMeta;
+import one.nem.lacerta.model.document.DocumentDetail;
+import one.nem.lacerta.model.document.path.DocumentPath;
+import one.nem.lacerta.model.document.tag.DocumentTag;
+
 public interface Document {
+
+    ArrayList<DocumentMeta> getRecentDocumentMetas(int limit);
+
+    ArrayList<DocumentMeta> getRecentDocumentMetas(int limit, int offset);
+
+    // ドキュメント詳細取得
+    DocumentDetail getDocumentDetail(String id);
+    DocumentDetail getDocumentDetailByMeta(DocumentMeta meta); // 簡単に使えるように
+
+
+
 }
