779a07ff263c0796ae30115eeb3ca7ddb6f6cd0c
ろむねこ <nem@nem.x0.to>
2023-12-14 10:16:07 +0900

    インタフェース実装

--- utils/src/main/java/one/nem/lacerta/utils/repository/SharedPrefUtils.java
+++ utils/src/main/java/one/nem/lacerta/utils/repository/SharedPrefUtils.java
@@ -1,4 +1,13 @@
 package one.nem.lacerta.utils.repository;
 
+import android.content.SharedPreferences;
+
 public interface SharedPrefUtils {
+
+    // Shared preferences editorの取得
+    SharedPreferences.Editor getEditor();
+
+    // TODO-rca: 名称をenumで管理する？
+    SharedPreferences.Editor getEditor(String name);
+
 }
