c59871300ad6e9bf254468103356bee74ac08d4c
ろむねこ <nem@nem.x0.to>
2023-12-14 14:54:12 +0900

    移動してなくなったインポート/Injectionを消した

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugPlayGroundFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugPlayGroundFragment.java
@@ -9,13 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import one.nem.lacerta.data.repository.Documents;
-
-import one.nem.lacerta.data.repository.SharedPref;
-
-import one.nem.lacerta.data.model.shared_pref.enums.SharedPrefType;
-
-import one.nem.lacerta.source.db.LacertaDatabase;
 
 import javax.inject.Inject;
 
@@ -23,15 +16,6 @@ import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint // HiltのDIを有効にするアノテーション
 public class DebugPlayGroundFragment extends Fragment {
-
-    @Inject // DI
-    Documents documents;
-
-    @Inject // DI
-    SharedPref sharedPref;
-
-    @Inject
-    LacertaDatabase lacertaDatabase; // TODO-rca: Repositoryを噛ませる
 
     public DebugPlayGroundFragment() {
         // Required empty public constructor
@@ -64,7 +48,5 @@ public class DebugPlayGroundFragment extends Fragment {
         // findViewById(R.id.hoge)とかは view.findViewById(R.id.hoge) と書き換える必要アリ
         // (viewは引数として受け取ってるviewなので、別メソッドに切り出したりするなら渡してあげる）
 
-        // ShardPrefの要素数をトーストする例
-        Toast.makeText(getContext(), "DBの要素数" + lacertaDatabase.repositoriesDao(), Toast.LENGTH_SHORT).show();
     }
 }
