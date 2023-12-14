960f159a31f623b3e46d13e40d8dcfece3940c27
ろむねこ <nem@nem.x0.to>
2023-12-14 10:18:24 +0900

    Impl実装

--- utils/src/main/java/one/nem/lacerta/utils/impl/SharedPrefUtilsImpl.java
+++ utils/src/main/java/one/nem/lacerta/utils/impl/SharedPrefUtilsImpl.java
@@ -1,17 +1,29 @@
 package one.nem.lacerta.utils.impl;
 
+
+import android.content.Context;
 import android.content.SharedPreferences;
 
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import one.nem.lacerta.utils.repository.SharedPrefUtils;
 
 public class SharedPrefUtilsImpl implements SharedPrefUtils{
+
+    private final Context applicationContext;
+
+    public SharedPrefUtilsImpl(@ApplicationContext Context applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Override
     public SharedPreferences.Editor getEditor() {
-        return null;
+        // Editorの取得
+        return applicationContext.getSharedPreferences("common", Context.MODE_PRIVATE).edit(); // TODO-rca: 決め打ちやめる?
     }
 
     @Override
     public SharedPreferences.Editor getEditor(String name) {
-        return null;
+        // Editorの取得
+        return applicationContext.getSharedPreferences(name, Context.MODE_PRIVATE).edit();
     }
 }
