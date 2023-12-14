2cf3898f6511ac1ec828146c05b4065f7f060d90
ろむねこ <nem@nem.x0.to>
2023-12-14 10:19:12 +0900

    Module実装

--- utils/src/main/java/one/nem/lacerta/utils/module/SharedPrefUtilsModule.java
+++ utils/src/main/java/one/nem/lacerta/utils/module/SharedPrefUtilsModule.java
@@ -1,4 +1,16 @@
 package one.nem.lacerta.utils.module;
 
-public class SharedPrefUtilsModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import one.nem.lacerta.utils.repository.SharedPrefUtils;
+import one.nem.lacerta.utils.impl.SharedPrefUtilsImpl;
+
+@Module
+@InstallIn(SingletonComponent.class) // TODO-rca: Singletonである必要があるのか検討する?
+abstract class SharedPrefUtilsModule {
+
+        @Binds
+        public abstract SharedPrefUtils bindSharedPrefUtils(SharedPrefUtilsImpl sharedPrefUtilsImpl);
 }
