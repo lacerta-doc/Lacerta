801d7e07376a7aa9d4a7b72e8a8a06f3310454bf
ろむねこ <nem@nem.x0.to>
2023-12-14 14:28:34 +0900

    Interface WIP

--- source/src/main/java/one/nem/lacerta/source/jgit/ManageRepo.java
+++ source/src/main/java/one/nem/lacerta/source/jgit/ManageRepo.java
@@ -1,4 +1,17 @@
 package one.nem.lacerta.source.jgit;
 
+import org.eclipse.jgit.lib.Repository;
+
 public interface ManageRepo {
+
+    // リポジトリ取得
+    Repository getRepository(String id);
+    // リポジトリ作成
+    Repository createRepository(String id);
+    // リポジトリ削除
+    void deleteRepository(String id);
+    // リポジトリ存在確認
+    boolean repositoryExists(String id);
+
+
 }
