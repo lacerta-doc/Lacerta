43b0dd4eed79241ea32d0dac6c4a2fcaf2736bda
r-ca <nem@nem.x0.to>
2023-12-16 14:20:26 +0900

    LoggerのModule作成

--- utils/src/main/java/one/nem/lacerta/utils/module/LacertaLoggerModule.java
+++ utils/src/main/java/one/nem/lacerta/utils/module/LacertaLoggerModule.java
@@ -1,4 +1,17 @@
 package one.nem.lacerta.utils.module;
 
-public class LacertaLoggerModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+import one.nem.lacerta.utils.LacertaLogger;
+import one.nem.lacerta.utils.impl.LacertaLoggerImpl;
+
+@Module
+@InstallIn(SingletonComponent.class) // ログの取得であまり負荷をかけたくないので
+abstract public class LacertaLoggerModule {
+
+    @Binds
+    public abstract LacertaLogger bindLacertaLogger(LacertaLoggerImpl lacertaLoggerImpl);
 }
