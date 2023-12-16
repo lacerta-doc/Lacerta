ac3523780fdebed994808d6ba38cfa96a74dd7b4
r-ca <nem@nem.x0.to>
2023-12-16 13:56:29 +0900

    Implにメソッド生成

--- source/src/main/java/one/nem/lacerta/source/jgit/impl/ActionRepoImpl.java
+++ source/src/main/java/one/nem/lacerta/source/jgit/impl/ActionRepoImpl.java
@@ -6,4 +6,43 @@ import one.nem.lacerta.source.jgit.ActionRepo;
 
 public class ActionRepoImpl implements ActionRepo{
 
+    @Override
+    public Repository setRepository(Repository repository) {
+        return null;
+    }
+
+    @Override
+    public Repository getRepository() {
+        return null;
+    }
+
+    @Override
+    public String getRepositoryName() {
+        return null;
+    }
+
+    @Override
+    public String[] getUnstagedFiles() {
+        return new String[0];
+    }
+
+    @Override
+    public String[] getStagedFiles() {
+        return new String[0];
+    }
+
+    @Override
+    public void stageFile(String path) {
+
+    }
+
+    @Override
+    public void unstageFile(String path) {
+
+    }
+
+    @Override
+    public void commit(String message) {
+
+    }
 }
