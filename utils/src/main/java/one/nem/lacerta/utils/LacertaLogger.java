c3b53776e4ba1a090887356c2cf7ef27cfc821f0
r-ca <nem@nem.x0.to>
2023-12-16 14:18:36 +0900

    Logger作成

--- utils/src/main/java/one/nem/lacerta/utils/LacertaLogger.java
+++ utils/src/main/java/one/nem/lacerta/utils/LacertaLogger.java
@@ -1,4 +1,12 @@
 package one.nem.lacerta.utils;
 
 public interface LacertaLogger {
+
+    void info(String tag, String message);
+    void warn(String tag, String message);
+    void error(String tag, String message);
+    void debug(String tag, String message);
+    void trace(String tag, String message);
+    void fatal(String tag, String message);
+
 }
--- utils/src/main/java/one/nem/lacerta/utils/impl/LacertaLoggerImpl.java
+++ utils/src/main/java/one/nem/lacerta/utils/impl/LacertaLoggerImpl.java
@@ -1,4 +1,37 @@
 package one.nem.lacerta.utils.impl;
 
-public class LacertaLoggerImpl {
+import android.util.Log;
+
+import one.nem.lacerta.utils.LacertaLogger;
+public class LacertaLoggerImpl implements LacertaLogger{
+
+    @Override
+    public void info(String tag, String message) {
+        Log.i(tag, message);
+    }
+
+    @Override
+    public void warn(String tag, String message) {
+        Log.w(tag, message);
+    }
+
+    @Override
+    public void error(String tag, String message) {
+        Log.e(tag, message);
+    }
+
+    @Override
+    public void debug(String tag, String message) {
+        Log.d(tag, message);
+    }
+
+    @Override
+    public void trace(String tag, String message) {
+        Log.v(tag, message);
+    }
+
+    @Override
+    public void fatal(String tag, String message) {
+        Log.wtf(tag, message);
+    }
 }
