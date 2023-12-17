81053009e0fb12938418c8b56191e1014b1ba434
r-ca <nem@nem.x0.to>
2023-12-18 01:29:22 +0900

    実装テスト

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/adapter/DebugMenuDocumentListItemAdapter.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/adapter/DebugMenuDocumentListItemAdapter.java
@@ -1,17 +1,24 @@
 package one.nem.lacerta.feature.debug.common.adapter;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import one.nem.lacerta.feature.debug.R;
 import one.nem.lacerta.feature.debug.common.model.DebugMenuDocumentListItem;
 
 public class DebugMenuDocumentListItemAdapter extends RecyclerView.Adapter<DebugMenuDocumentListItemAdapter.DebugMenuDocumentListItemViewHolder> {
 
     private List<DebugMenuDocumentListItem> debugMenuDocumentListItems;
+
+    public DebugMenuDocumentListItemAdapter(List<DebugMenuDocumentListItem> debugMenuDocumentListItems) {
+        this.debugMenuDocumentListItems = debugMenuDocumentListItems;
+    }
 
     @NonNull
     @Override
@@ -30,5 +37,17 @@ public class DebugMenuDocumentListItemAdapter extends RecyclerView.Adapter<Debug
     }
 
     public class DebugMenuDocumentListItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        TextView description;
+        TextView updatedAt;
+        public DebugMenuDocumentListItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.document_list_item_title);
+            description = itemView.findViewById(R.id.document_list_item_description);
+            updatedAt = itemView.findViewById(R.id.document_list_item_updated_at);
+
+        }
     }
 }
