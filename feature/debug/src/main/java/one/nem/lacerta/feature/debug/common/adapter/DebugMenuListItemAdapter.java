e3ebe25cdd366e52a39b956e738bdd4aa1c0e183
r-ca <nem@nem.x0.to>
2023-12-17 14:16:56 +0900

    推移イベント呼ぶようにした

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/adapter/DebugMenuListItemAdapter.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/adapter/DebugMenuListItemAdapter.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -32,6 +33,10 @@ public class DebugMenuListItemAdapter extends RecyclerView.Adapter<DebugMenuList
         DebugMenuListItem item = debugMenuListItems.get(position);
         holder.title.setText(item.getTitle());
         holder.description.setText(item.getDescription());
+
+        holder.itemView.setOnClickListener( v -> {
+            Navigation.findNavController(v).navigate(item.getDestinationId());
+        });
     }
 
     @Override
