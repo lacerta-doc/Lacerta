f8a1aa1e77997a3bc59ed733e1a30628ac280442
r-ca <nem@nem.x0.to>
2023-12-17 13:16:14 +0900

    データモデル作成

---
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/model/SettingMenuItem.java
@@ -0,0 +1,11 @@
+package one.nem.lacerta.feature.debug.common.model;
+
+import android.content.Intent;
+
+import androidx.navigation.ActivityNavigator;
+
+public class SettingMenuItem {
+    private String title;
+    private String description;
+    private String destinationId;
+}
