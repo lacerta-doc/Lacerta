334954b6a84927df6fc31bcaaa97141c4ada170f
r-ca <nem@nem.x0.to>
2023-12-17 13:55:35 +0900

    データモデル作成

---
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/model/DebugMenuListItem.java
@@ -0,0 +1,10 @@
+package one.nem.lacerta.feature.debug.common.model;
+
+public class DebugMenuListItem {
+
+    private String title;
+    private String description;
+    private String destinationId; // Navigation destination ID
+    private boolean enabled;
+
+}
