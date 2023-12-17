14ba4f0c9382b26a9fa3c7acbcd5033858c96d38
r-ca <nem@nem.x0.to>
2023-12-17 14:18:50 +0900

    RecyclerView実装WIP

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuTopFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuTopFragment.java
@@ -3,10 +3,16 @@ package one.nem.lacerta.feature.debug;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import one.nem.lacerta.feature.debug.common.model.DebugMenuListItem;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,6 +39,13 @@ public class DebugMenuTopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debug_menu_top, container, false);
+        View view = inflater.inflate(R.layout.fragment_debug_menu_top, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.debug_menu_recycler_view);
+
+        List<DebugMenuListItem> debugMenuListItems = new ArrayList<>();
+        debugMenuListItems.add(new DebugMenuListItem("Meta Data", "View meta data", R.id.action_debugMenuTopFragment_to_debugMenuMetaDataFragment, true));
+
+        return view;
     }
 }
