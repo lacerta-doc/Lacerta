eff8e2037a11b71aa657f11ab4a8bbb6a0fdb719
r-ca <nem@nem.x0.to>
2023-12-17 19:29:20 +0900

    WIP

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocumentTesterTopFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocumentTesterTopFragment.java
@@ -3,10 +3,16 @@ package one.nem.lacerta.feature.debug;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import one.nem.lacerta.feature.debug.common.model.DebugMenuListItem;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,6 +39,15 @@ public class DebugMenuDocumentTesterTopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debug_menu_document_tester_top, container, false);
+        View view = inflater.inflate(R.layout.fragment_debug_menu_document_tester_top, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.debug_menu_document_tester_recycler_view);
+        recyclerView.setLayoutManager(new androidx.recyclerview.widget.LinearLayoutManager(getContext()));
+
+        List<DebugMenuListItem> debugMenuDocTesterListItems = new ArrayList<>();
+
+        // WIP
+
+        return view;
     }
 }
