4fb93c41e8fba86b381dfdb05316922b1d950eca
r-ca <nem@nem.x0.to>
2023-12-18 01:32:44 +0900

    import追加, Inject

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocumentTesterListFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocumentTesterListFragment.java
@@ -3,10 +3,17 @@ package one.nem.lacerta.feature.debug;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import javax.inject.Inject;
+
+import one.nem.lacerta.data.Document;
+import one.nem.lacerta.feature.debug.common.adapter.DebugMenuDocumentListItemAdapter;
+import one.nem.lacerta.feature.debug.common.model.DebugMenuDocumentListItem;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +21,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DebugMenuDocumentTesterListFragment extends Fragment {
+
+    @Inject
+    Document document;
+
     public DebugMenuDocumentTesterListFragment() {
         // Required empty public constructor
     }
@@ -34,6 +45,13 @@ public class DebugMenuDocumentTesterListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debug_menu_document_tester_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_debug_menu_document_tester_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_document_list);
+        recyclerView.setLayoutManager(new androidx.recyclerview.widget.LinearLayoutManager(getContext()));
+
+
+
+        return view;
     }
 }
