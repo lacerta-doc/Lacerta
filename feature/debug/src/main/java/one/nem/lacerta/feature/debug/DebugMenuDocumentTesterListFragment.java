37bc68fef9db2b36c366b6cd87fcb88fe881733d
r-ca <nem@nem.x0.to>
2023-12-18 01:37:08 +0900

    アノテーション追加

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocumentTesterListFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocumentTesterListFragment.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 import javax.inject.Inject;
 
+import dagger.hilt.android.AndroidEntryPoint;
 import one.nem.lacerta.data.Document;
 import one.nem.lacerta.feature.debug.common.adapter.DebugMenuDocumentListItemAdapter;
 import one.nem.lacerta.feature.debug.common.model.DebugMenuDocumentListItem;
@@ -24,6 +25,8 @@ import one.nem.lacerta.model.document.DocumentMeta;
  * Use the {@link DebugMenuDocumentTesterListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+@AndroidEntryPoint
 public class DebugMenuDocumentTesterListFragment extends Fragment {
 
     @Inject
