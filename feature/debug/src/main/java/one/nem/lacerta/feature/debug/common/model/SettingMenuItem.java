b7e29233cd8aecb5fae568706947bc5b8936705d
r-ca <nem@nem.x0.to>
2023-12-17 13:21:18 +0900

    Getter/Setter追加

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/model/SettingMenuItem.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/model/SettingMenuItem.java
@@ -8,4 +8,35 @@ public class SettingMenuItem {
     private String title;
     private String description;
     private String destinationId;
+
+    public SettingMenuItem(String title, String description, String destinationId) {
+        this.title = title;
+        this.description = description;
+        this.destinationId = destinationId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDestinationId(String destinationId) {
+        this.destinationId = destinationId;
+    }
+
 }
