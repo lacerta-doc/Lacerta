1cf28d599759e81f229b4a1298515227c9bcde8f
r-ca <nem@nem.x0.to>
2023-12-17 13:55:56 +0900

    Getter/Setter作成

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/model/DebugMenuListItem.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/common/model/DebugMenuListItem.java
@@ -7,4 +7,32 @@ public class DebugMenuListItem {
     private String destinationId; // Navigation destination ID
     private boolean enabled;
 
+    public DebugMenuListItem(String title, String description, String destinationId, boolean enabled) {
+        this.title = title;
+        this.description = description;
+        this.destinationId = destinationId;
+        this.enabled = enabled;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
 }
