310944f51ee91b18d8bbc3e1c5149654c190ba09
ろむねこ <nem@nem.x0.to>
2023-12-21 14:45:35 +0900

    ImplクラスInit

---
+++ source/src/main/java/one/nem/lacerta/source/file/impl/RepoFileManagerImpl.java
@@ -0,0 +1,21 @@
+package one.nem.lacerta.source.file.impl;
+
+import android.graphics.Bitmap;
+import one.nem.lacerta.source.file.RepoFileManager;
+
+public class RepoFileManagerImpl implements RepoFileManager{
+    @Override
+    public void initRepoDir() {
+
+    }
+
+    @Override
+    public void addImageByIndex(Bitmap bitmap, int index) {
+
+    }
+
+    @Override
+    public void deleteImageByIndex(int index) {
+
+    }
+}
