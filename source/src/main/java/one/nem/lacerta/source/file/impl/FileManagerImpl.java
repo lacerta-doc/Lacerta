7e7d72f903dc4c54cff939bf4399ea80288a09d6
ろむねこ <nem@nem.x0.to>
2023-12-21 14:54:45 +0900

    Impl実装 WIP

--- source/src/main/java/one/nem/lacerta/source/file/impl/FileManagerImpl.java
+++ source/src/main/java/one/nem/lacerta/source/file/impl/FileManagerImpl.java
@@ -3,30 +3,67 @@ package one.nem.lacerta.source.file.impl;
 import android.graphics.Bitmap;
 
 import java.nio.file.Path;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import one.nem.lacerta.source.file.FileManager;
 
 public class FileManagerImpl implements FileManager {
 
+    // RootDir
     private Path rootDir;
 
+    // CurrentDir
+    private Path currentDir;
+
+    // Internal Methods
+    private Path convertPath(String path) {
+        return null;
+    }
+
+    @Inject
+    public FileManagerImpl(Path rootDir) {
+        // TODO-rca: 未実装
+    }
+
     @Override
-    public void initRepoDir() {
+    public Path getRootDir() {
+        return null;
+    }
+
+    @Override
+    public void changeDir(String dirName) {
 
     }
 
     @Override
-    public void addImageByIndex(Bitmap bitmap, int index) {
+    public void backDir() {
 
     }
 
     @Override
-    public void deleteImageByIndex(int index) {
+    public void backRootDir() {
 
     }
 
-    // Internal
-    private void createDir() {
+    @Override
+    public List<String> getDirList() {
+        return null;
+    }
+
+    @Override
+    public List<String> getFileList() {
+        return null;
+    }
+
+    @Override
+    public void createDir() {
+
+    }
+
+    @Override
+    public void removeDir() {
 
     }
 }
