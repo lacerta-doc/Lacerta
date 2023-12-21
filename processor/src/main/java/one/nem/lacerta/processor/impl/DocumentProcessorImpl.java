413c42fdc11ba326c5623a7c491ba63655e3bf4c
ろむねこ <nem@nem.x0.to>
2023-12-21 14:27:28 +0900

    Impl実装 WIP

--- processor/src/main/java/one/nem/lacerta/processor/impl/DocumentProcessorImpl.java
+++ processor/src/main/java/one/nem/lacerta/processor/impl/DocumentProcessorImpl.java
@@ -1,4 +1,47 @@
 package one.nem.lacerta.processor.impl;
 
-public class DocumentProcessorImpl {
+import android.graphics.Bitmap;
+
+import one.nem.lacerta.processor.DocumentProcessor;
+
+public class DocumentProcessorImpl implements DocumentProcessor{
+    @Override
+    public void addNewPageToLast(Bitmap bitmap) {
+
+    }
+
+    @Override
+    public void addNewPagesToLast(Bitmap[] bitmaps) {
+
+    }
+
+    @Override
+    public void addNewPageAfterIndex(Bitmap bitmap, int index) {
+
+    }
+
+    @Override
+    public void addNewPageBeforeIndex(Bitmap bitmap, int index) {
+
+    }
+
+    @Override
+    public void removePageAtIndex(int index) {
+
+    }
+
+    @Override
+    public void updatePageAtIndex(Bitmap bitmap, int index) {
+
+    }
+
+    @Override
+    public Bitmap getPageAtIndex(int index) {
+        return null;
+    }
+
+    @Override
+    public int getPageCount() {
+        return 0;
+    }
 }
