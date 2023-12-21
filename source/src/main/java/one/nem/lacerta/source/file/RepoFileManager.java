aff7af76d4bbb62515d78560db18cb5d615ee8a8
ろむねこ <nem@nem.x0.to>
2023-12-21 14:44:36 +0900

    Repository実装

--- source/src/main/java/one/nem/lacerta/source/file/RepoFileManager.java
+++ source/src/main/java/one/nem/lacerta/source/file/RepoFileManager.java
@@ -1,7 +1,13 @@
 package one.nem.lacerta.source.file;
 
+import android.graphics.Bitmap;
+
+import org.eclipse.jgit.lib.Repository;
+
 public interface RepoFileManager {
 
-    void
+    void initRepoDir();
+    void addImageByIndex(Bitmap bitmap, int index);
+    void deleteImageByIndex(int index);
 
 }
