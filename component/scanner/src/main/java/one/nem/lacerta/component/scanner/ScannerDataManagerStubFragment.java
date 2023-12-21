cf012daddf48004e39261cdd4aab1b0441ed9a60
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-12-21 12:34:46 +0900

    Revert "fix typo"
    This reverts commit 60d13ac9a9c19bc55e7164c6e7ffed4e472da25e.

--- component/scanner/src/main/java/one/nem/lacerta/component/scanner/ScannerDataManagerStubFragment.java
+++ component/scanner/src/main/java/one/nem/lacerta/component/scanner/ScannerDataManagerStubFragment.java
@@ -57,7 +57,7 @@ public class ScannerDataManagerStubFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scanner_data_manager_stub, container, false);
+        return inflater.inflate(R.layout.fragment_scanner_databa_manager_stub, container, false);
     }
 
     @Override
