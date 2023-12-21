bbad45fbbdaf820571a8f623a86eb95bdb61c340
ろむねこ <nem@nem.x0.to>
2023-12-21 15:03:44 +0900

    WIP

---
+++ source/src/main/java/one/nem/lacerta/source/file/model/DirectoryItem.java
@@ -0,0 +1,11 @@
+package one.nem.lacerta.source.file.model;
+
+public class DirectoryItem {
+
+    String name;
+    String path;
+    boolean isDirectory;
+    String extension;
+    long size;
+
+}
