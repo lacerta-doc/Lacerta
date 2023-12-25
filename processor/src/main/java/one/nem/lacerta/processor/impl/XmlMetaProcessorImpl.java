c8517ad814a3b8b3b710cfbdb0e38dc9a633c6df
r-ca <nem@nem.x0.to>
2023-12-25 14:49:04 +0900

    Impl

--- processor/src/main/java/one/nem/lacerta/processor/impl/XmlMetaProcessorImpl.java
+++ processor/src/main/java/one/nem/lacerta/processor/impl/XmlMetaProcessorImpl.java
@@ -1,4 +1,17 @@
 package one.nem.lacerta.processor.impl;
 
-public class XmlMetaProcessorImpl {
+import one.nem.lacerta.processor.XmlMetaProcessor;
+import one.nem.lacerta.processor.model.XmlMetaModel;
+
+public class XmlMetaProcessorImpl implements XmlMetaProcessor{
+
+    @Override
+    public void saveXmlMeta(XmlMetaModel xmlMetaModel) {
+
+    }
+
+    @Override
+    public XmlMetaModel loadXmlMeta() {
+        return null;
+    }
 }
