2154e67178e9233d1a266eccb74deece361fc46b
r-ca <nem@nem.x0.to>
2023-12-25 11:38:14 +0900

    WIP

--- processor/src/main/java/one/nem/lacerta/processor/impl/DocumentProcessorImpl.java
+++ processor/src/main/java/one/nem/lacerta/processor/impl/DocumentProcessorImpl.java
@@ -2,20 +2,31 @@ package one.nem.lacerta.processor.impl;
 
 import android.graphics.Bitmap;
 
+import java.nio.file.Path;
+
+import javax.inject.Inject;
+
 import one.nem.lacerta.processor.DocumentProcessor;
 
 import one.nem.lacerta.model.document.DocumentDetail;
 import one.nem.lacerta.model.document.DocumentMeta;
 
+import one.nem.lacerta.source.file.FileManager;
+
 public class DocumentProcessorImpl implements DocumentProcessor{
 
     private DocumentDetail documentDetail;
 
+    // Injection
+    @Inject
+    FileManager fileManager;
 
+    // Internal utils
 
     @Override
     public void addNewPageToLast(Bitmap bitmap) {
-
+        Path path = documentDetail.getPath().getFullPath();
+//        fileManager.saveBitmap(path, bitmap);
     }
 
     @Override
