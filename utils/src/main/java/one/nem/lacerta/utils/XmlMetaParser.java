4875394417f339c7a63307e4b3757216ad23c4ac
r-ca <nem@nem.x0.to>
2023-12-25 15:33:56 +0900

    XMLMetaのパーサーを作成 Repository WIP

--- utils/src/main/java/one/nem/lacerta/utils/XmlMetaParser.java
+++ utils/src/main/java/one/nem/lacerta/utils/XmlMetaParser.java
@@ -1,4 +1,12 @@
 package one.nem.lacerta.utils;
 
+import one.nem.lacerta.model.document.internal.XmlMetaModel;
+
 public interface XmlMetaParser {
+
+    // ドキュメントのメタデータをXMLファイルから読み込む
+    XmlMetaModel parse(String xml);
+    // ドキュメントのメタデータをXMLファイルに書き込む
+    String serialize(XmlMetaModel meta);
+
 }
