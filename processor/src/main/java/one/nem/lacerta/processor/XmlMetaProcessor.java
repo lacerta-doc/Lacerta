ffb6ebb3582c194a31c894a20fe005f4f5506479
r-ca <nem@nem.x0.to>
2023-12-25 14:46:45 +0900

    Repository追加

--- processor/src/main/java/one/nem/lacerta/processor/XmlMetaProcessor.java
+++ processor/src/main/java/one/nem/lacerta/processor/XmlMetaProcessor.java
@@ -1,4 +1,10 @@
 package one.nem.lacerta.processor;
 
+import one.nem.lacerta.processor.model.XmlMetaModel;
+import one.nem.lacerta.processor.model.XmlMetaPageModel;
+
 public interface XmlMetaProcessor {
+
+    void saveXmlMeta(XmlMetaModel xmlMetaModel);
+    XmlMetaModel loadXmlMeta();
 }
