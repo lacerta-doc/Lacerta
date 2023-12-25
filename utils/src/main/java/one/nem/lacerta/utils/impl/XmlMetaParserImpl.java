b98fa18cae2cf85a402aebe94749e45c2542984c
r-ca <nem@nem.x0.to>
2023-12-25 15:34:17 +0900

    XMLMetaのパーサーを作成 Impl WIP

--- utils/src/main/java/one/nem/lacerta/utils/impl/XmlMetaParserImpl.java
+++ utils/src/main/java/one/nem/lacerta/utils/impl/XmlMetaParserImpl.java
@@ -1,4 +1,16 @@
 package one.nem.lacerta.utils.impl;
 
-public class XmlMetaParserImpl {
+import one.nem.lacerta.model.document.internal.XmlMetaModel;
+import one.nem.lacerta.utils.XmlMetaParser;
+
+public class XmlMetaParserImpl implements XmlMetaParser{
+    @Override
+    public XmlMetaModel parse(String xml) {
+        return null;
+    }
+
+    @Override
+    public String serialize(XmlMetaModel meta) {
+        return null;
+    }
 }
