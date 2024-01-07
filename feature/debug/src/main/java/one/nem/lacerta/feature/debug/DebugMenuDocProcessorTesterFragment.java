c9eda4837b1ccde47b52ac5b892e9c99a5c1d702
r-ca <nem@nem.x0.to>
2024-01-07 16:26:46 +0900

    いろいろ

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocProcessorTesterFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuDocProcessorTesterFragment.java
@@ -35,6 +35,8 @@ public class DebugMenuDocProcessorTesterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debug_menu_doc_processor_tester, container, false);
+        View view = inflater.inflate(R.layout.fragment_debug_menu_doc_processor_tester, container, false);
+
+        return view;
     }
 }
