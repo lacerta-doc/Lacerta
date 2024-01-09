3ed835ef2f203815eefef6c33b9f6b9fe97cdfa2
r-ca <nem@nem.x0.to>
2024-01-09 11:34:12 +0900

    既存の実装を削除

--- source/src/main/java/one/nem/lacerta/source/file/FileManager.java
+++ source/src/main/java/one/nem/lacerta/source/file/FileManager.java
@@ -12,48 +12,5 @@ import java.util.List;
 /** @noinspection unused*/
 public interface FileManager {
 
-    Path getRootDir();
-    Path getCurrentDir();
-
-    void changeDir(String dirName); //cd
-    void changeDir(Path path); //cd
-    void backDir(); //cd ..
-    void backRootDir(); //cd /
-    List<Path> getList();
-    void createDir(String dirName);
-    void createDir(Path path);
-    void removeDir(String dirName);
-    void removeDir(Path path);
-
-    File createFile(String fileName);
-    void removeFile(String fileName);
-
-    File getFile(String fileName);
-    File getFile(Path path);
-
-    String loadText(String fileName);
-    String loadText(Path path);
-
-    void saveText(String text, String fileName);
-    void saveText(String text, Path path);
-
-    void saveDocument(Document document, String fileName);
-    void saveDocument(Document document, Path path);
-
-    Document loadDocument(String fileName);
-    Document loadDocument(Path path);
-
-
-    boolean isExist(Path path);
-    boolean isExist(String fileName);
-
-    void autoCreateDir(Path path);
-    void autoCreateDir(String dirName);
-
-    void autoCreateToCurrentDir();
-
-    void saveBitmapAtCurrent(Bitmap bitmap, String fileName);
-    Bitmap loadBitmap(Path path);
-    void removeBitmap(Path path);
 
 }
