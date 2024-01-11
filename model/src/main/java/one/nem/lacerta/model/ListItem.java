19dfa070fd8a0e519d0d2b7e5cf49b62549c017c
ろむねこ <nem@nem.x0.to>
2024-01-11 14:52:30 +0900

    ListItemモデル作成, コンストラクタ定義, Getter

--- model/src/main/java/one/nem/lacerta/model/ListItem.java
+++ model/src/main/java/one/nem/lacerta/model/ListItem.java
@@ -1,4 +1,35 @@
 package one.nem.lacerta.model;
 
+import android.graphics.drawable.Drawable;
+
 public class ListItem {
+
+    String title;
+    String description;
+    Drawable icon;
+    String itemId;
+
+    public ListItem(String title, String description, Drawable icon, String itemId) {
+        this.title = title;
+        this.description = description;
+        this.icon = icon;
+        this.itemId = itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Drawable getIcon() {
+        return icon;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
 }
