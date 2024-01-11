5dcb27a2e9bbd15563fae23ccc36980bcb4e1aa6
ろむねこ <nem@nem.x0.to>
2024-01-11 15:04:42 +0900

    アイコンIDを追加

--- model/src/main/java/one/nem/lacerta/model/ListItemType.java
+++ model/src/main/java/one/nem/lacerta/model/ListItemType.java
@@ -1,4 +1,17 @@
 package one.nem.lacerta.model;
 
 public enum ListItemType {
+
+    ITEM_TYPE_FOLDER(one.nem.lacerta.shared.ui.R.drawable.folder_24px),
+    ITEM_TYPE_DOCUMENT(one.nem.lacerta.shared.ui.R.drawable.description_24px);
+
+    private int iconId;
+
+    ListItemType(int iconId) {
+        this.iconId = iconId;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
 }
