81ceaaf0d6fa6ff6857ae352ed6b69d40c2a1c02
ろむねこ <nem@nem.x0.to>
2024-01-12 12:16:11 +0900

    init

--- feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuLibraryItemListPageFragment.java
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuLibraryItemListPageFragment.java
@@ -35,6 +35,9 @@ public class DebugMenuLibraryItemListPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debug_menu_library_item_list_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_debug_menu_library_item_list_page, container, false);
+
+
+        return view;
     }
 }
