ca61d2c055c04ebebe21845c03b450b58867c37f
ろむねこ <nem@nem.x0.to>
2024-01-12 09:54:38 +0900

    ListItemモデルをラッピングするモデルを作成

--- model/src/main/java/one/nem/lacerta/model/LibraryItemList.java
+++ model/src/main/java/one/nem/lacerta/model/LibraryItemList.java
@@ -1,4 +1,28 @@
 package one.nem.lacerta.model;
 
+import java.util.ArrayList;
+
 public class LibraryItemList {
+
+    String pageTitle;
+    String pageId;
+    ArrayList<ListItem> listItems;
+
+    public LibraryItemList(String pageTitle, String pageId, ArrayList<ListItem> listItems) {
+        this.pageTitle = pageTitle;
+        this.pageId = pageId;
+        this.listItems = listItems;
+    }
+
+    public String getPageTitle() {
+        return pageTitle;
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public ArrayList<ListItem> getListItems() {
+        return listItems;
+    }
 }
