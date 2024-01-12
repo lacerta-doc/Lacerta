f9375038403a5e6c9bcf414643fbed89bea5f1a6
ろむねこ <nem@nem.x0.to>
2024-01-12 10:18:04 +0900

    インタフェース設計 WIP

--- data/src/main/java/one/nem/lacerta/data/LacertaLibrary.java
+++ data/src/main/java/one/nem/lacerta/data/LacertaLibrary.java
@@ -3,18 +3,20 @@ package one.nem.lacerta.data;
 import one.nem.lacerta.model.ListItemType;
 
 import one.nem.lacerta.model.LibraryItemPage;
+import one.nem.lacerta.model.document.DocumentDetail;
 
 public interface LacertaLibrary {
 
     // Get History
-
     LibraryItemPage getRecentDocument(int limit);
     LibraryItemPage getRecentDocument(int limit, int offset);
-    LibraryItemPage getRecentDocument(int limit, int offset, ListItemType type);
 
-    // Get List
+    // Get Library page
+    LibraryItemPage getLibraryPage(int limit);
+    LibraryItemPage getLibraryPage(int limit, int offset);
+    LibraryItemPage getLibraryPage(String pageId, int limit);
+    LibraryItemPage getLibraryPage(String pageId, int limit, int offset);
 
-    LibraryItemPage getDocumentList(int limit);
-
-
+    // GetDocument
+    DocumentDetail getDocumentDetailById(String id);
 }
