01f0d74ab645b21cf8a0ecab978c05b86bad361c
ろむねこ <nem@nem.x0.to>
2024-01-12 10:27:36 +0900

    継承, Impl生成

--- data/src/main/java/one/nem/lacerta/data/impl/LacertaLibraryStubImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/LacertaLibraryStubImpl.java
@@ -1,4 +1,41 @@
 package one.nem.lacerta.data.impl;
+import one.nem.lacerta.data.LacertaLibrary;
+import one.nem.lacerta.model.LibraryItemPage;
+import one.nem.lacerta.model.document.DocumentDetail;
 
-public class LacertaLibraryStubImpl {
+public class LacertaLibraryStubImpl implements LacertaLibrary {
+    @Override
+    public LibraryItemPage getRecentDocument(int limit) {
+        return null;
+    }
+
+    @Override
+    public LibraryItemPage getRecentDocument(int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    public LibraryItemPage getLibraryPage(int limit) {
+        return null;
+    }
+
+    @Override
+    public LibraryItemPage getLibraryPage(int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    public LibraryItemPage getLibraryPage(String pageId, int limit) {
+        return null;
+    }
+
+    @Override
+    public LibraryItemPage getLibraryPage(String pageId, int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    public DocumentDetail getDocumentDetailById(String id) {
+        return null;
+    }
 }
