c346170a9da83a5ca86fe4d826c5d92cafd68bb0
ろむねこ <nem@nem.x0.to>
2024-01-12 12:27:51 +0900

    HiltModuleのアノテーション修正

--- data/src/main/java/one/nem/lacerta/data/module/LacertaLibraryModule.java
+++ data/src/main/java/one/nem/lacerta/data/module/LacertaLibraryModule.java
@@ -3,13 +3,15 @@ package one.nem.lacerta.data.module;
 import dagger.Binds;
 import dagger.Module;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.FragmentComponent;
 import dagger.hilt.migration.DisableInstallInCheck;
 
 import one.nem.lacerta.data.LacertaLibrary;
 import one.nem.lacerta.data.impl.LacertaLibraryStubImpl;
 
 @Module
-@DisableInstallInCheck
+// Fragmentにinstall
+@InstallIn(FragmentComponent.class)
 abstract public class LacertaLibraryModule {
 
     @Binds
