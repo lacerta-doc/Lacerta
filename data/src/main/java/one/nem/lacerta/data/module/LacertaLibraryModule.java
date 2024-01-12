dc4879c900a10cef074860e5e33af55f0fe4e776
ろむねこ <nem@nem.x0.to>
2024-01-12 12:14:01 +0900

    HiltModule作成

--- data/src/main/java/one/nem/lacerta/data/module/LacertaLibraryModule.java
+++ data/src/main/java/one/nem/lacerta/data/module/LacertaLibraryModule.java
@@ -1,4 +1,18 @@
 package one.nem.lacerta.data.module;
 
-public class LacertaLibraryModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.migration.DisableInstallInCheck;
+
+import one.nem.lacerta.data.LacertaLibrary;
+import one.nem.lacerta.data.impl.LacertaLibraryStubImpl;
+
+@Module
+@DisableInstallInCheck
+abstract public class LacertaLibraryModule {
+
+    @Binds
+    public abstract LacertaLibrary bindLacertaLibrary(LacertaLibraryStubImpl impl);
+
 }
