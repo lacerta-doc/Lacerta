3930ec6d9c1d5178a0984797c23f8f9b0d2e4500
ろむねこ <nem@nem.x0.to>
2024-01-12 09:54:48 +0900

    WIP

--- data/src/main/java/one/nem/lacerta/data/LacertaLibrary.java
+++ data/src/main/java/one/nem/lacerta/data/LacertaLibrary.java
@@ -1,4 +1,21 @@
 package one.nem.lacerta.data;
 
+import java.util.ArrayList;
+
+import one.nem.lacerta.model.ListItem;
+import one.nem.lacerta.model.ListItemType;
+
 public interface LacertaLibrary {
+
+    // Get History
+
+    ArrayList<ListItem> getRecentDocument(int limit);
+    ArrayList<ListItem> getRecentDocument(int limit, int offset);
+    ArrayList<ListItem> getRecentDocument(int limit, int offset, ListItemType type);
+
+    // Get List
+
+    ArrayList<ListItem> getDocumentList(int limit);
+
+
 }
