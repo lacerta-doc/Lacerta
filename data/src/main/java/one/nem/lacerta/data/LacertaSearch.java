1d24467c09c343f5bbd453ddd38927a458274802
r-ca <nem@nem.x0.to>
2024-01-13 14:29:58 +0900

    インタフェースの戻り値を修正

--- data/src/main/java/one/nem/lacerta/data/LacertaSearch.java
+++ data/src/main/java/one/nem/lacerta/data/LacertaSearch.java
@@ -1,11 +1,13 @@
 package one.nem.lacerta.data;
 
+import java.util.ArrayList;
+
 import one.nem.lacerta.model.ListItem;
 
 public interface LacertaSearch {
 
-    ListItem autoSearch(String query, int limit);
+    ArrayList<ListItem> autoSearch(String query, int limit);
 
-    ListItem autoSearch(String query, int limit, int offset);
+    ArrayList<ListItem> autoSearch(String query, int limit, int offset);
 
 }
