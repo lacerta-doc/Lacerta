82d0187a11950f62fb1d997d14e96983581df041
r-ca <nem@nem.x0.to>
2024-01-13 14:34:07 +0900

    LacertaLibraryの実装を流用して取得できるように

--- data/src/main/java/one/nem/lacerta/data/impl/LacertaSearchStubImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/LacertaSearchStubImpl.java
@@ -2,18 +2,30 @@ package one.nem.lacerta.data.impl;
 
 import java.util.ArrayList;
 
+import javax.inject.Inject;
+
 import one.nem.lacerta.data.LacertaSearch;
 import one.nem.lacerta.model.ListItem;
 
+import one.nem.lacerta.data.LacertaLibrary;
+
 public class LacertaSearchStubImpl implements LacertaSearch {
+
+    private LacertaLibrary library;
+
+
+    @Inject
+    public LacertaSearchStubImpl(LacertaLibrary library) {
+        this.library = library;
+    }
 
     @Override
     public ArrayList<ListItem> autoSearch(String query, int limit) {
-        return null;
+        return library.getLibraryPage(limit).getListItems();
     }
 
     @Override
     public ArrayList<ListItem> autoSearch(String query, int limit, int offset) {
-        return null;
+        return library.getLibraryPage(limit, offset).getListItems();
     }
 }
