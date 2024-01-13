3c66480f0fe951fc845bc22fb656251d8d174da0
r-ca <nem@nem.x0.to>
2024-01-13 14:35:25 +0900

    受け取ったクエリをコンソールに吐き出すようにした

--- data/src/main/java/one/nem/lacerta/data/impl/LacertaSearchStubImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/LacertaSearchStubImpl.java
@@ -8,24 +8,31 @@ import one.nem.lacerta.data.LacertaSearch;
 import one.nem.lacerta.model.ListItem;
 
 import one.nem.lacerta.data.LacertaLibrary;
+import one.nem.lacerta.utils.LacertaLogger;
 
 public class LacertaSearchStubImpl implements LacertaSearch {
 
     private LacertaLibrary library;
+    private LacertaLogger logger;
 
 
     @Inject
-    public LacertaSearchStubImpl(LacertaLibrary library) {
+    public LacertaSearchStubImpl(LacertaLibrary library, LacertaLogger logger) {
         this.library = library;
+        this.logger = logger;
     }
 
     @Override
     public ArrayList<ListItem> autoSearch(String query, int limit) {
+        logger.debug("SearchStub", "autoSearch");
+        logger.debug("SearchStub", "query: " + query);
         return library.getLibraryPage(limit).getListItems();
     }
 
     @Override
     public ArrayList<ListItem> autoSearch(String query, int limit, int offset) {
+        logger.debug("SearchStub", "autoSearch");
+        logger.debug("SearchStub", "query: " + query);
         return library.getLibraryPage(limit, offset).getListItems();
     }
 }
