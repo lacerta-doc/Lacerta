04651f3b8edc43b7c88cfb603e55fbe5444403a5
r-ca <nem@nem.x0.to>
2024-01-13 15:12:03 +0900

    実装 WIP

--- model/src/main/java/one/nem/lacerta/model/PublicPath.java
+++ model/src/main/java/one/nem/lacerta/model/PublicPath.java
@@ -1,23 +1,47 @@
 package one.nem.lacerta.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PublicPath {
     /*
      * ユーザーが扱うパス(内部パスの代替)
      */
 
-    String[] path;
+    List<String> path = new ArrayList<String>();
 
     public PublicPath() {
-        this.path = new String[0];
     }
 
-    public PublicPath load(String[] path) {
+    public PublicPath(List<String> path) {
         this.path = path;
+    }
+
+    private void add(String path) {
+        this.path.add(path);
+    }
+
+    public PublicPath resolve(String path) {
+        if (path.equals("..")) {
+            this.path.remove(this.path.size() - 1);
+        } else {
+            add(path);
+        }
         return this;
     }
 
-    public PublicPath load(String path) {
-        this.path = path.split("/");
+    public PublicPath resolve(List<String> path) {
+        for (String p : path) {
+            resolve(p);
+        }
         return this;
+    }
+
+    public List<String> getPath() {
+        return path;
+    }
+
+    public String getStringPath() {
+        return String.join("/", path);
     }
 }
