e848f40b8b01efc77d1dcc7179a12586a3ef7d9f
r-ca <nem@nem.x0.to>
2024-01-13 15:51:09 +0900

    エンティティ実装 WIP

--- source/src/main/java/one/nem/lacerta/source/database/entity/VcsLogEntity.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/VcsLogEntity.java
@@ -1,4 +1,42 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "vcs_log")
 public class VcsLogEntity {
+
+    /**
+     * イベントID
+     */
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    public String id;
+
+    /**
+     * ドキュメントID
+     */
+    @ColumnInfo(name = "document_id")
+    public String documentId;
+
+    /**
+     * ブランチ名
+     */
+    @ColumnInfo(name = "branch_name")
+    public String branchName;
+
+    /**
+     * 発生日時
+     */
+    @ColumnInfo(name = "created_at")
+    public Date createdAt;
+
+    /**
+     * 発生アクション
+     */
+    @ColumnInfo(name = "action")
+    public String action;
 }
