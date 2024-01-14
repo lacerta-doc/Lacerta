0d4f0686b72ab26ae1996ad087da36e3b762da1c
r-ca <nem@nem.x0.to>
2024-01-14 16:13:09 +0900

    DocumentDetailにPagesを追加

--- model/src/main/java/one/nem/lacerta/model/document/DocumentDetail.java
+++ model/src/main/java/one/nem/lacerta/model/document/DocumentDetail.java
@@ -3,10 +3,13 @@ package one.nem.lacerta.model.document;
 import org.eclipse.jgit.lib.Repository;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Date;
 
 import one.nem.lacerta.model.document.path.DocumentPath;
 import one.nem.lacerta.model.document.DocumentMeta;
+
+import one.nem.lacerta.model.document.page.Page;
 
 /**
  * ドキュメントの詳細データ
@@ -18,12 +21,22 @@ public class DocumentDetail {
      */
     DocumentMeta meta;
 
+    /**
+     * ドキュメントのページ(Pageインスタンスのリスト)
+     */
+    ArrayList<Page> pages;
+
     // Constructor
     public DocumentDetail() {
     }
 
     public DocumentDetail(DocumentMeta meta) {
         this.meta = meta;
+    }
+
+    public DocumentDetail(DocumentMeta meta, ArrayList<Page> pages) {
+        this.meta = meta;
+        this.pages = pages;
     }
 
     // Getter
@@ -35,6 +48,13 @@ public class DocumentDetail {
         return meta;
     }
 
+    /**
+     * ドキュメントのページ(Pageインスタンスのリスト)を取得する
+     */
+    public ArrayList<Page> getPages() {
+        return pages;
+    }
+
     // Setter
 
     /**
@@ -43,6 +63,14 @@ public class DocumentDetail {
      */
     public void setMeta(DocumentMeta meta) {
         this.meta = meta;
+    }
+
+    /**
+     * ドキュメントのページ(Pageインスタンスのリスト)を設定する
+     * @param pages Pageインスタンスのリスト
+     */
+    public void setPages(ArrayList<Page> pages) {
+        this.pages = pages;
     }
 
 }
