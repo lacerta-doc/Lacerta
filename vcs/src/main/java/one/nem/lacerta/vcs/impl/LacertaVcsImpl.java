f98614c5f23aea37e1f782ac107f3e1c97f3616e
r-ca <nem@nem.x0.to>
2024-01-14 13:00:29 +0900

    Implクラスコンストラクタ実装 WIP

--- vcs/src/main/java/one/nem/lacerta/vcs/impl/LacertaVcsImpl.java
+++ vcs/src/main/java/one/nem/lacerta/vcs/impl/LacertaVcsImpl.java
@@ -1,8 +1,32 @@
 package one.nem.lacerta.vcs.impl;
 
+import javax.inject.Inject;
+
+import dagger.assisted.Assisted;
+import dagger.assisted.AssistedInject;
+import one.nem.lacerta.source.database.LacertaDatabase;
+import one.nem.lacerta.utils.LacertaLogger;
 import one.nem.lacerta.vcs.LacertaVcs;
 
 public class LacertaVcsImpl implements LacertaVcs {
+
+    // TAG
+    private static final String TAG = LacertaVcsImpl.class.getSimpleName();
+
+    String documentId;
+
+    LacertaDatabase database;
+
+    LacertaLogger logger;
+
+    @AssistedInject
+    public LacertaVcsImpl(LacertaLogger logger, LacertaDatabase database, @Assisted String documentId) {
+        this.logger = logger;
+        this.database = database;
+        this.documentId = documentId;
+        logger.debug(TAG, "LacertaVcsImpl constructor");
+    }
+
     @Override
     public void updatePage(int index, String fileName) {
 
@@ -20,6 +44,6 @@ public class LacertaVcsImpl implements LacertaVcs {
 
     @Override
     public void printLog() {
-
+        logger.debug(TAG, "printLog");
     }
 }
