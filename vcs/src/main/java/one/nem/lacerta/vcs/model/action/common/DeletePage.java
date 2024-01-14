dc65cdbac816994cc86add6b894a67828c54f1e5
r-ca <nem@nem.x0.to>
2024-01-14 12:25:16 +0900

    DeletePageモデル作成, コンストラクタ, Getter作成

---
+++ vcs/src/main/java/one/nem/lacerta/vcs/model/action/common/DeletePage.java
@@ -0,0 +1,17 @@
+package one.nem.lacerta.vcs.model.action.common;
+
+public class DeletePage {
+
+        private int index;
+
+        public DeletePage() {
+        }
+
+        public DeletePage(int index) {
+            this.index = index;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+}
