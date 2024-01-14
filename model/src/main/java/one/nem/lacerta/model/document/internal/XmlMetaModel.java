cd5a588545a2e32b611fc5a8346f7972798a8f91
r-ca <nem@nem.x0.to>
2024-01-14 16:34:41 +0900

    XmlMetaModel更新

--- model/src/main/java/one/nem/lacerta/model/document/internal/XmlMetaModel.java
+++ model/src/main/java/one/nem/lacerta/model/document/internal/XmlMetaModel.java
@@ -4,12 +4,8 @@ import java.util.ArrayList;
 
 public class XmlMetaModel {
 
-    String title;
-    String author;
-    String description;
-//    Date created;
-//    Date updated;
-    String defaultBranch;
+    String revisionId;
+
     ArrayList<XmlMetaPageModel> pages;
 
     // Constructor
@@ -17,31 +13,15 @@ public class XmlMetaModel {
     public XmlMetaModel() {
     }
 
-    public XmlMetaModel(String title, String author, String description, String defaultBranch, ArrayList<XmlMetaPageModel> pages) {
-        this.title = title;
-        this.author = author;
-        this.description = description;
-        this.defaultBranch = defaultBranch;
+    public XmlMetaModel(String revisionId, ArrayList<XmlMetaPageModel> pages) {
+        this.revisionId = revisionId;
         this.pages = pages;
     }
 
-
     // Getter
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getDefaultBranch() {
-        return defaultBranch;
+    public String getRevisionId() {
+        return revisionId;
     }
 
     public ArrayList<XmlMetaPageModel> getPages() {
@@ -50,24 +30,11 @@ public class XmlMetaModel {
 
     // Setter
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setDefaultBranch(String defaultBranch) {
-        this.defaultBranch = defaultBranch;
+    public void setRevisionId(String revisionId) {
+        this.revisionId = revisionId;
     }
 
     public void setPages(ArrayList<XmlMetaPageModel> pages) {
         this.pages = pages;
     }
-
 }
