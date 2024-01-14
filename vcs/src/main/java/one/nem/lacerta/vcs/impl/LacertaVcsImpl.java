b040e2572c90b3d8271366175e2d0b44f25351f6
r-ca <nem@nem.x0.to>
2024-01-14 12:55:09 +0900

    Implクラス作成, Impl生成

---
+++ vcs/src/main/java/one/nem/lacerta/vcs/impl/LacertaVcsImpl.java
@@ -0,0 +1,25 @@
+package one.nem.lacerta.vcs.impl;
+
+import one.nem.lacerta.vcs.LacertaVcs;
+
+public class LacertaVcsImpl implements LacertaVcs {
+    @Override
+    public void updatePage(int index, String fileName) {
+
+    }
+
+    @Override
+    public void insertPage(int index, String fileName) {
+
+    }
+
+    @Override
+    public void deletePage(int index) {
+
+    }
+
+    @Override
+    public void printLog() {
+
+    }
+}
