88546fe89a4c7dd01e8a745e81d1690616f0493f
r-ca <nem@nem.x0.to>
2024-01-14 11:29:58 +0900

    Databaseに追加 WIP

--- source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
+++ source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
@@ -7,13 +7,15 @@ import androidx.room.RoomDatabase;
 import one.nem.lacerta.source.database.entity.TagEntity;
 import one.nem.lacerta.source.database.entity.DocumentEntity;
 import one.nem.lacerta.source.database.entity.LibraryEntity;
+import one.nem.lacerta.source.database.entity.VcsRevEntity;
+import one.nem.lacerta.source.database.entity.VcsLogEntity;
 
 // Daos
 import one.nem.lacerta.source.database.dao.TagDao;
 import one.nem.lacerta.source.database.dao.DocumentDao;
 import one.nem.lacerta.source.database.dao.LibraryDao;
 
-@Database(entities = {TagEntity.class, DocumentEntity.class, LibraryEntity.class}, version = 1)
+@Database(entities = {TagEntity.class, DocumentEntity.class, LibraryEntity.class, VcsRevEntity.class, VcsLogEntity.class}, version = 1)
 public abstract class LacertaDatabase extends RoomDatabase {
     public abstract TagDao tagDao();
     public abstract DocumentDao documentDao();
