3eb94cade01dd6292db41836ab035c9dd9ce8700
r-ca <nem@nem.x0.to>
2024-01-14 12:54:29 +0900

    インタフェース実装 WIP

--- vcs/src/main/java/one/nem/lacerta/vcs/LacertaVcs.java
+++ vcs/src/main/java/one/nem/lacerta/vcs/LacertaVcs.java
@@ -1,4 +1,15 @@
 package one.nem.lacerta.vcs;
 
 public interface LacertaVcs {
+
+    // Actions
+    public void updatePage(int index, String fileName);
+
+    public void insertPage(int index, String fileName);
+
+    public void deletePage(int index);
+
+    // debug
+    public void printLog();
+
 }
