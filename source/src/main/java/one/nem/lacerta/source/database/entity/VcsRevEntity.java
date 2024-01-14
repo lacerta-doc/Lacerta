53b3ebebefea7029f4285076c516978291acc5e5
r-ca <nem@nem.x0.to>
2024-01-14 11:28:51 +0900

    Rev実装

--- source/src/main/java/one/nem/lacerta/source/database/entity/VcsRevEntity.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/VcsRevEntity.java
@@ -1,5 +1,48 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.List;
+
+@Entity(tableName = "vcs_rev")
 public class VcsRevEntity {
-    // TODO-rca: 未実装
+
+    /**
+     * リビジョンID
+     */
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    public String id;
+
+    /**
+     * ドキュメントID
+     */
+    @ColumnInfo(name = "document_id")
+    public String documentId;
+
+    /**
+     * ブランチ名
+     */
+    @ColumnInfo(name = "branch_name")
+    public String branchName;
+
+    /**
+     * コミットメッセージ
+     */
+    @ColumnInfo(name = "commit_message")
+    public String commitMessage;
+
+    /**
+     * コミット日時
+     */
+    @ColumnInfo(name = "commit_date")
+    public String commitDate;
+
+    /**
+     * 含まれるLogのID
+     */
+    @ColumnInfo(name = "log_ids")
+    public List<String> logIds;
 }
