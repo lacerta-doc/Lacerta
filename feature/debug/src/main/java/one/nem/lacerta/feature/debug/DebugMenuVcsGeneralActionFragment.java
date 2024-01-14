4c40f9926e90cbc99348271cdcfd9b1c901fed5f
r-ca <nem@nem.x0.to>
2024-01-14 13:45:43 +0900

    フラグメント作成, 引数関係を削除

---
+++ feature/debug/src/main/java/one/nem/lacerta/feature/debug/DebugMenuVcsGeneralActionFragment.java
@@ -0,0 +1,42 @@
+package one.nem.lacerta.feature.debug;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DebugMenuVcsGeneralActionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DebugMenuVcsGeneralActionFragment extends Fragment {
+
+    public DebugMenuVcsGeneralActionFragment() {
+        // Required empty public constructor
+    }
+
+    public static DebugMenuVcsGeneralActionFragment newInstance() {
+        DebugMenuVcsGeneralActionFragment fragment = new DebugMenuVcsGeneralActionFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_debug_menu_vcs_general_action, container, false);
+
+        return view;
+    }
+}
