75c07001445f0a1cbb06bb10c410e54207e33207
ろむねこ <nem@nem.x0.to>
2024-01-15 14:36:10 +0900

    レイアウト編集, 処理init

--- component/scanner/src/main/java/one/nem/lacerta/component/scanner/ScannerManagerFragment.java
+++ component/scanner/src/main/java/one/nem/lacerta/component/scanner/ScannerManagerFragment.java
@@ -15,33 +15,17 @@ import android.view.ViewGroup;
  */
 public class ScannerManagerFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final boolean DEFAULT_SINGLE_PAGE = false;
+    private boolean singlePage;
 
     public ScannerManagerFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScannerManagerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScannerManagerFragment newInstance(String param1, String param2) {
+    public static ScannerManagerFragment newInstance(boolean singlePage) {
         ScannerManagerFragment fragment = new ScannerManagerFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putBoolean("singlePage", singlePage);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +34,7 @@ public class ScannerManagerFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            singlePage = getArguments().getBoolean("singlePage", DEFAULT_SINGLE_PAGE);
         }
     }
 
@@ -60,5 +43,14 @@ public class ScannerManagerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scanner_manager, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_start_scan).setOnClickListener(v -> {
+
+        });
     }
 }
