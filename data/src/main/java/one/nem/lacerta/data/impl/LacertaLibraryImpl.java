e0d63122bdf695e7d45ee58bf2ccad6862678a80
ろむねこ <nem@nem.x0.to>
2024-01-17 12:52:13 +0900

    getRecentDocument実装

--- data/src/main/java/one/nem/lacerta/data/impl/LacertaLibraryImpl.java
+++ data/src/main/java/one/nem/lacerta/data/impl/LacertaLibraryImpl.java
@@ -1,14 +1,19 @@
 package one.nem.lacerta.data.impl;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import one.nem.lacerta.data.LacertaLibrary;
 import one.nem.lacerta.model.LibraryItemPage;
 import one.nem.lacerta.model.ListItem;
+import one.nem.lacerta.model.ListItemType;
 import one.nem.lacerta.model.document.DocumentDetail;
 import one.nem.lacerta.source.database.LacertaDatabase;
+import one.nem.lacerta.source.database.common.DateTypeConverter;
+import one.nem.lacerta.source.database.entity.DocumentEntity;
 import one.nem.lacerta.utils.LacertaLogger;
 
 public class LacertaLibraryImpl implements LacertaLibrary {
@@ -21,12 +26,23 @@ public class LacertaLibraryImpl implements LacertaLibrary {
 
     @Override
     public ArrayList<ListItem> getRecentDocument(int limit) {
-        return null;
+        List<DocumentEntity> documentEntities = database.documentDao().getRecentDocument(limit);
+
+        ArrayList<ListItem> listItems = new ArrayList<>();
+        for (DocumentEntity documentEntity : documentEntities) {
+            ListItem listItem = new ListItem();
+            listItem.setItemType(ListItemType.ITEM_TYPE_DOCUMENT);
+            listItem.setTitle(documentEntity.title);
+            listItem.setDescription(DateFormat.getDateInstance().format(documentEntity.updatedAt));
+            listItem.setItemId(documentEntity.id);
+            listItems.add(listItem);
+        }
+        return listItems;
     }
 
     @Override
     public ArrayList<ListItem> getRecentDocument(int limit, int offset) {
-        return null;
+        return null; // TODO-rca: Implement
     }
 
     @Override
