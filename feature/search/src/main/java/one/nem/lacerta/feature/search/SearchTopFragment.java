1e3a3262a4c3c646d20b1db7341debae00e0ed64
Fuchimoto1234 <it232246@trident.ac.jp>
2024-01-19 12:11:56 +0900

    search UI

--- feature/search/src/main/java/one/nem/lacerta/feature/search/SearchTopFragment.java
+++ feature/search/src/main/java/one/nem/lacerta/feature/search/SearchTopFragment.java
@@ -1,12 +1,22 @@
 package one.nem.lacerta.feature.search;
 
+import android.app.ActionBar;
+import android.app.LauncherActivity;
 import android.os.Bundle;
 
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,8 +26,17 @@ import android.view.ViewGroup;
 public class SearchTopFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String TAG = SearchTopFragment.class.getSimpleName();
+    private final SearchTopFragment self = this;
+
+    private SearchView searchView;
+    private String searchWord;
+
+
     private static final String ARG_PARAM1 = "param1";
+
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
@@ -37,6 +56,8 @@ public class SearchTopFragment extends Fragment {
      * @return A new instance of fragment SearchTopFragment.
      */
     // TODO: Rename and change types and number of parameters
+
+
     public static SearchTopFragment newInstance(String param1, String param2) {
         SearchTopFragment fragment = new SearchTopFragment();
         Bundle args = new Bundle();
@@ -60,5 +81,76 @@ public class SearchTopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_top, container, false);
+
     }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Menuの設定
+
+
+
+
+
+        // ActionViewの取得
+
+
+        // 虫眼鏡アイコンを最初表示するかの設定
+        this.searchView.setIconifiedByDefault(true);
+
+        // Submitボタンを表示するかどうか
+        this.searchView.setSubmitButtonEnabled(false);
+
+
+        if (!this.searchWord.equals("")) {
+            // TextView.setTextみたいなもの
+            this.searchView.setQuery(this.searchWord, false);
+        } else {
+            String queryHint = self.getResources().getString(R.string.hello_blank_fragment);
+            // placeholderみたいなもの
+            this.searchView.setQueryHint(queryHint);
+        }
+        this.searchView.setOnQueryTextListener(self.onQueryTextListener);
+    }
+
+    private SearchView.OnQueryTextListener onQueryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String searchWord) {
+            // SubmitボタンorEnterKeyを押されたら呼び出されるメソッド
+            return self.setSearchWord(searchWord);
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            // 入力される度に呼び出される
+            return false;
+        }
+    };
+    private boolean setSearchWord(String searchWord) {
+
+
+        if (searchWord != null && !searchWord.equals("")) {
+            // searchWordがあることを確認
+            this.searchWord = searchWord;
+        }
+        // 虫眼鏡アイコンを隠す
+        this.searchView.setIconified(false);
+        // SearchViewを隠す
+        this.searchView.onActionViewCollapsed();
+        // Focusを外す
+        this.searchView.clearFocus();
+        return false;
+    }
+
 }
+
+
+    interface LacertaSearch {
+        ArrayList<LauncherActivity.ListItem> autoSearch(String query, int limit);
+
+        ArrayList<LauncherActivity.ListItem> autoSearch(String query, int limit, int offset);
+    }
+
+
+
+
