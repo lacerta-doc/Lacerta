646a97e77bd56f0b5528f90cfff785ce85a1e6ae
r-ca <nem@nem.x0.to>
2024-01-20 17:21:48 +0900

    Arg取得 WIP

--- component/viewer/src/main/java/one/nem/lacerta/component/viewer/ViewerMainActivity.java
+++ component/viewer/src/main/java/one/nem/lacerta/component/viewer/ViewerMainActivity.java
@@ -1,14 +1,42 @@
 package one.nem.lacerta.component.viewer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import one.nem.lacerta.utils.LacertaLogger;
+
+@AndroidEntryPoint
 public class ViewerMainActivity extends AppCompatActivity {
+
+    LacertaLogger logger;
+
+    @Inject
+    public ViewerMainActivity(LacertaLogger logger) {
+        this.logger = logger;
+        logger.debug(TAG, "ViewerMainActivity created");
+    }
+
+
+    // Variables
+    private static final String TAG = "ViewerMainActivity";
+    String documentId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +48,16 @@ public class ViewerMainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+        try {
+            documentId = intent.getStringExtra("documentId");
+        }
+        catch (Exception e) {
+            logger.error(TAG, "Failed to get documentId from intent");
+            logger.error(TAG, "Searchable Error code: " + "f64c21a2-391f-4c40-92f6-183da459de21");
+            Toast.makeText(this, "Failed to get documentId from intent", Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
