db26e639e6fad19145a8ae7d0ef655716c0bcfaf
r-ca <nem@nem.x0.to>
2024-01-20 15:27:39 +0900

    テスト用にアイテム追加

--- feature/setting/src/main/java/one/nem/lacerta/setting/SettingTopFragment.java
+++ feature/setting/src/main/java/one/nem/lacerta/setting/SettingTopFragment.java
@@ -2,11 +2,18 @@ package one.nem.lacerta.setting;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import one.nem.lacerta.setting.model.SettingListItem;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +42,24 @@ public class SettingTopFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting_top, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting_top, container, false);
+
+        // Setting items
+        ArrayList<SettingListItem> settingListItems = new ArrayList<>();
+        settingListItems.add(
+                new SettingListItem(
+                        "About",
+                        "About this app",
+                        ContextCompat.getDrawable(getContext(), one.nem.lacerta.shared.ui.R.drawable.save_24px),
+                        R.id.action_settingTopFragment_to_settingAboutPageFragment
+                )
+        );
+
+        // Init RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.setting_item_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new ListItemAdapter(settingListItems));
+
+        return view;
     }
 }
