882207682fe3e82cb15bb1f1db87565022d4826e
r-ca <nem@nem.x0.to>
2024-01-21 18:56:15 +0900

    FeatureSwitchクラス作成 WIP

---
+++ utils/src/main/java/one/nem/lacerta/utils/FeatureSwitch.java
@@ -0,0 +1,14 @@
+package one.nem.lacerta.utils;
+
+public class FeatureSwitch {
+
+    public static class FeatureMaster {
+        public static boolean enableSearch = true;
+    }
+
+    public static class Setting {
+        public static boolean showDisplayMenu = false;
+        public static boolean showDataMenu = false;
+        public static boolean showScanMenu = false;
+    }
+}
