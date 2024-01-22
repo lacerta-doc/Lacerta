2821ad8552a8779a82c78c913bb7b7bf60b65552
r-ca <nem@nem.x0.to>
2024-01-23 04:33:57 +0900

    Import Optimize

--- feature/setting/src/main/java/one/nem/lacerta/setting/SettingTopFragment.java
+++ feature/setting/src/main/java/one/nem/lacerta/setting/SettingTopFragment.java
@@ -1,15 +1,14 @@
 package one.nem.lacerta.setting;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
