ad05e3db44a4614a5ad32befb7d5cdf41ef06392
r-ca <nem@nem.x0.to>
2024-01-23 04:32:53 +0900

    Import Optimize

--- feature/home/src/main/java/one/nem/lacerta/feature/home/HomeTopFragment.java
+++ feature/home/src/main/java/one/nem/lacerta/feature/home/HomeTopFragment.java
@@ -4,40 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import one.nem.lacerta.component.viewer.ViewerMainActivity;
-import one.nem.lacerta.data.Document;
 import one.nem.lacerta.data.LacertaLibrary;
-import one.nem.lacerta.model.ListItem;
-import one.nem.lacerta.model.document.DocumentMeta;
 import one.nem.lacerta.utils.FeatureSwitch;
-import one.nem.lacerta.utils.Store;
 
 
 /**
