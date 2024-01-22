e104b3d8ff2574303d2367a75e933092216f4889
r-ca <nem@nem.x0.to>
2024-01-23 00:45:15 +0900

    Revert "引数削除など"
    This reverts commit 4714e3d8cbe79e011eb58992abe8c4fddd1e845c.

--- feature/library/src/main/java/one/nem/lacerta/feature/library/LibraryContainerFragment.java
+++ feature/library/src/main/java/one/nem/lacerta/feature/library/LibraryContainerFragment.java
@@ -15,13 +15,33 @@ import android.view.ViewGroup;
  */
 public class LibraryContainerFragment extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
     public LibraryContainerFragment() {
         // Required empty public constructor
     }
 
-    public static LibraryContainerFragment newInstance() {
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LibraryContainerFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LibraryContainerFragment newInstance(String param1, String param2) {
         LibraryContainerFragment fragment = new LibraryContainerFragment();
         Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -29,6 +49,10 @@ public class LibraryContainerFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
