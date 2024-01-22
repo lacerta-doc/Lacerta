ed5e6994935c66b9a77f8a81c0120076225eaf65
r-ca <nem@nem.x0.to>
2024-01-23 02:53:53 +0900

    WIP

--- feature/setting/src/main/java/one/nem/lacerta/setting/SettingScanPageFragment.java
+++ feature/setting/src/main/java/one/nem/lacerta/setting/SettingScanPageFragment.java
@@ -2,8 +2,12 @@ package one.nem.lacerta.setting;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.preference.PreferenceFragmentCompat;
 
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +17,15 @@ import android.view.ViewGroup;
  * Use the {@link SettingScanPageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingScanPageFragment extends Fragment {
+public class SettingScanPageFragment extends PreferenceFragmentCompat {
 
     public SettingScanPageFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        addPreferencesFromResource(R.xml.scan_preferences);
     }
 
     public static SettingScanPageFragment newInstance() {
@@ -26,16 +35,4 @@ public class SettingScanPageFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting_scan_page, container, false);
-    }
 }
