d8d8a5932bf4f80f350f021c517fdd4c1b36e13b
r-ca <nem@nem.x0.to>
2024-01-23 00:45:14 +0900

    Revert "WIP"
    This reverts commit 113a2fd3d2a09084711fe00a4112744faa6febb5.

--- feature/library/src/main/java/one/nem/lacerta/feature/library/LibraryContainerFragment.java
+++ feature/library/src/main/java/one/nem/lacerta/feature/library/LibraryContainerFragment.java
@@ -3,22 +3,17 @@ package one.nem.lacerta.feature.library;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import one.nem.lacerta.model.FragmentNavigation;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link LibraryContainerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LibraryContainerFragment extends Fragment implements FragmentNavigation {
-
-    FragmentContainerView fragmentContainerView;
+public class LibraryContainerFragment extends Fragment {
 
     public LibraryContainerFragment() {
         // Required empty public constructor
@@ -41,32 +36,5 @@ public class LibraryContainerFragment extends Fragment implements FragmentNaviga
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_library_container, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        fragmentContainerView = view.findViewById(R.id.fragmentContainerView);
-    }
-
-    @Override
-    public void navigateToFragment(Fragment fragment) {
-        this.navigateToFragment(fragment, true);
-    }
-
-    @Override
-    public void navigateToFragment(Fragment fragment, boolean addToBackStack) {
-        this.navigateToFragment(fragment, addToBackStack, false);
-    }
-
-    @Override
-    public void navigateToFragment(Fragment fragment, boolean addToBackStack, boolean clearBackStack) {
-        if (fragmentContainerView != null) {
-        }
-    }
-
-    @Override
-    public void navigateToFragmentAlternate(Fragment fragment, boolean addToBackStack) {
-
     }
 }
