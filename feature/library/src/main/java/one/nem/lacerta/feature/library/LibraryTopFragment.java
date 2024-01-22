e1129bc0093e96edbbe1ede7c5a94cb9413b74de
ろむねこ <nem@nem.x0.to>
2024-01-22 11:02:01 +0900

    ToolBar削除

--- feature/library/src/main/java/one/nem/lacerta/feature/library/LibraryTopFragment.java
+++ feature/library/src/main/java/one/nem/lacerta/feature/library/LibraryTopFragment.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
