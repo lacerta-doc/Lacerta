1212d5111bf479fcc29297045b60bd17983a9452
ろむねこ <nem@nem.x0.to>
2024-01-25 16:22:54 +0900

    複合主キーの指定方法を修正

--- source/src/main/java/one/nem/lacerta/source/database/entity/ToxiDocumentTagEntity.java
+++ source/src/main/java/one/nem/lacerta/source/database/entity/ToxiDocumentTagEntity.java
@@ -1,18 +1,18 @@
 package one.nem.lacerta.source.database.entity;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "toxi_document_tag")
+@Entity(primaryKeys = {"document_id", "tag_id"}, tableName = "toxi_document_tag")
 public class ToxiDocumentTagEntity {
 
-    // 複合主キー
-    @PrimaryKey
+    @NonNull
     @ColumnInfo(name = "document_id")
     public String documentId; // ドキュメントID
 
-    @PrimaryKey
+    @NonNull
     @ColumnInfo(name = "tag_id")
     public String tagId; // タグID
 }
