26b0994343cc484fa0aa9c64fbdced96eb23e6d3
ろむねこ <nem@nem.x0.to>
2024-01-25 16:30:31 +0900

    プロパティ, コンストラクタ, Getter/Setter実装

--- feature/setting/src/main/java/one/nem/lacerta/setting/model/TagListItem.java
+++ feature/setting/src/main/java/one/nem/lacerta/setting/model/TagListItem.java
@@ -1,4 +1,51 @@
 package one.nem.lacerta.setting.model;
 
 public class TagListItem {
+
+    String tagId;
+    String tagName;
+    String tagDescription;
+    String tagColor;
+
+    public TagListItem(String tagId, String tagName, String tagDescription, String tagColor) {
+        this.tagId = tagId;
+        this.tagName = tagName;
+        this.tagDescription = tagDescription;
+        this.tagColor = tagColor;
+    }
+
+    public TagListItem() {
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public String getTagDescription() {
+        return tagDescription;
+    }
+
+    public void setTagDescription(String tagDescription) {
+        this.tagDescription = tagDescription;
+    }
+
+    public String getTagColor() {
+        return tagColor;
+    }
+
+    public void setTagColor(String tagColor) {
+        this.tagColor = tagColor;
+    }
 }
