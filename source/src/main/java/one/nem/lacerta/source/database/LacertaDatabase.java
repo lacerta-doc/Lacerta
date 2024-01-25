b91f0ae022c1d0504b13324026a01364b50a8990
ろむねこ <nem@nem.x0.to>
2024-01-25 15:47:35 +0900

    Entity追加

--- source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
+++ source/src/main/java/one/nem/lacerta/source/database/LacertaDatabase.java
@@ -9,6 +9,7 @@ import one.nem.lacerta.source.database.entity.FolderEntity;
 import one.nem.lacerta.source.database.entity.TagEntity;
 import one.nem.lacerta.source.database.entity.DocumentEntity;
 import one.nem.lacerta.source.database.entity.LibraryEntity;
+import one.nem.lacerta.source.database.entity.ToxiDocumentTagEntity;
 import one.nem.lacerta.source.database.entity.VcsRevEntity;
 import one.nem.lacerta.source.database.entity.VcsLogEntity;
 
@@ -19,7 +20,7 @@ import one.nem.lacerta.source.database.dao.LibraryDao;
 import one.nem.lacerta.source.database.dao.VcsRevDao;
 import one.nem.lacerta.source.database.dao.VcsLogDao;
 
-@Database(entities = {TagEntity.class, DocumentEntity.class, LibraryEntity.class, VcsRevEntity.class, VcsLogEntity.class, FolderEntity.class}, version = 4)
+@Database(entities = {TagEntity.class, DocumentEntity.class, LibraryEntity.class, VcsRevEntity.class, VcsLogEntity.class, FolderEntity.class, ToxiDocumentTagEntity.class}, version = 6)
 public abstract class LacertaDatabase extends RoomDatabase {
     public abstract TagDao tagDao();
     public abstract DocumentDao documentDao();
