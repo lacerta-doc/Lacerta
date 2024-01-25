05915a2f4edef0e4f0cf3e6f147a5de45d0f1724
ろむねこ <nem@nem.x0.to>
2024-01-25 15:49:53 +0900

    Dao実装

--- source/src/main/java/one/nem/lacerta/source/database/dao/ToxiDocumentTagDao.java
+++ source/src/main/java/one/nem/lacerta/source/database/dao/ToxiDocumentTagDao.java
@@ -10,4 +10,28 @@ import one.nem.lacerta.source.database.entity.ToxiDocumentTagEntity;
 
 @Dao
 public interface ToxiDocumentTagDao {
+
+    @Query("SELECT * FROM toxi_document_tag WHERE document_id = :documentId")
+    List<ToxiDocumentTagEntity> findByDocumentId(String documentId);
+
+    @Query("SELECT * FROM toxi_document_tag WHERE tag_id = :tagId")
+    List<ToxiDocumentTagEntity> findByTagId(String tagId);
+
+    @Insert
+    void insert(ToxiDocumentTagEntity toxiDocumentTag);
+
+    @Insert
+    void insertAll(ToxiDocumentTagEntity... toxiDocumentTags);
+
+    @Insert
+    void insertAll(List<ToxiDocumentTagEntity> toxiDocumentTags);
+
+    @Query("DELETE FROM toxi_document_tag WHERE document_id = :documentId")
+    void deleteByDocumentId(String documentId);
+
+    @Query("DELETE FROM toxi_document_tag WHERE tag_id = :tagId")
+    void deleteByTagId(String tagId);
+
+    @Query("DELETE FROM toxi_document_tag WHERE document_id = :documentId AND tag_id = :tagId")
+    void deleteByDocumentIdAndTagId(String documentId, String tagId);
 }
