ae0f023e0e9b7dc439147f775e2a7e961a1e6d0b
ろむねこ <nem@nem.x0.to>
2024-01-25 15:48:22 +0900

    Dao実装 WIP

---
+++ source/src/main/java/one/nem/lacerta/source/database/dao/ToxiDocumentTagDao.java
@@ -0,0 +1,13 @@
+package one.nem.lacerta.source.database.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import one.nem.lacerta.source.database.entity.ToxiDocumentTagEntity;
+
+@Dao
+public interface ToxiDocumentTagDao {
+}
