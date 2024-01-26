c4bd3d761e43f180f13d4024bf8e96454dff6620
ろむねこ <nem@nem.x0.to>
2024-01-26 15:00:53 +0900

    タイトルなどのGetter/Setter実装

--- component/common/src/main/java/one/nem/lacerta/component/common/LacertaCreateTagDialog.java
+++ component/common/src/main/java/one/nem/lacerta/component/common/LacertaCreateTagDialog.java
@@ -11,6 +11,36 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class LacertaCreateTagDialog extends DialogFragment {
 
+    private String title;
+
+    private String message;
+
+    private String positiveButtonText;
+
+    private String negativeButtonText;
+
+
+
+    public LacertaCreateTagDialog setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public LacertaCreateTagDialog setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public LacertaCreateTagDialog setPositiveButtonText(String positiveButtonText) {
+        this.positiveButtonText = positiveButtonText;
+        return this;
+    }
+
+    public LacertaCreateTagDialog setNegativeButtonText(String negativeButtonText) {
+        this.negativeButtonText = negativeButtonText;
+        return this;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
