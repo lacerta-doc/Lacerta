bd1ea74c4c72b0f8aa077d365b4e1d8201e48a59
r-ca <nem@nem.x0.to>
2024-01-28 12:35:16 +0900

    PagerAdapter実装WIP

---
+++ component/viewer/src/main/java/one/nem/lacerta/component/viewer/ViewerViewPagerAdapter.java
@@ -0,0 +1,26 @@
+package one.nem.lacerta.component.viewer;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewerViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewerViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
