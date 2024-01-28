425616c3359a329ee5613d4075396b59dee6d66a
r-ca <nem@nem.x0.to>
2024-01-29 00:12:28 +0900

    コメント

--- component/common/src/main/java/one/nem/lacerta/component/common/model/DocumentTagApplyTagDialogExtendedModel.java
+++ component/common/src/main/java/one/nem/lacerta/component/common/model/DocumentTagApplyTagDialogExtendedModel.java
@@ -2,6 +2,8 @@ package one.nem.lacerta.component.common.model;
 
 import one.nem.lacerta.model.document.tag.DocumentTag;
 
+// TODO-rca: クラス名が長すぎ
+
 /**
  * DocumentTagを設定するダイアログで使うための拡張モデル
  * チェックボックスの状態を保持するように
